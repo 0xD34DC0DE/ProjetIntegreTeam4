27aaba66e78e5583a69d7cf476ee06dc00f00347
William Jean Mireault <williamjeanmireault@gmail.com>
2021-09-13 12:49:30 -0400

    Renamed front-end -> frontend, added simple home with axios exemple

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,6 +2,7 @@ import './App.css';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import Login from './components/Login';
 import Register from './components/Register';
+import Home from './components/Home';
 
 function App() {
   return (
@@ -10,6 +11,7 @@ function App() {
         <Switch>
           <Route path="/login" exact component={Login}/>
           <Route path="/register" exact component={Register}/>
+          <Route path="/home" exact component={Home}/>
         </Switch>
       </div>
     </Router>
