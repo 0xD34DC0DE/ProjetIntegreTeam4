f2ea5fa40722353d138b0ea4e0052e9eb85cd71a
William Jean Mireault <williamjeanmireault@gmail.com>
2021-09-14 15:38:16 -0400

    Installed new version of material-ui

---
+++ front-end/src/App.js
@@ -0,0 +1,17 @@
+import './App.css';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import Home from './components/Home';
+
+function App() {
+  return (
+    <Router>
+      <div className="App"> 
+        <Switch>
+          <Route path="/home" exact component={Home}/>
+        </Switch>
+      </div>
+    </Router>
+  );
+}
+
+export default App;
