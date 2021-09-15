0aa8ed28f886b88ce42d7a39d90f671fe00d9ae9
Moussa <Moussa.Keita2411@gmail.com>
2021-09-15 15:53:40 -0400

    replacing argument in login method for a dto

--- backend/src/main/java/com/team4/backend/service/UserService.java
+++ backend/src/main/java/com/team4/backend/service/UserService.java
@@ -1,5 +1,6 @@
 package com.team4.backend.service;
 
+import com.team4.backend.model.dto.AuthRequest;
 import com.team4.backend.repository.UserRepository;
 import com.team4.backend.util.JwtUtil;
 import com.team4.backend.util.PBKDF2Encoder;
@@ -21,8 +22,8 @@ public class UserService {
     @Autowired
     private JwtUtil jwtUtil;
 
-    public Mono<String> login(String email, String password){
-        return userRepository.findByEmailAndPassword(email, pbkdf2Encoder.encode(password))
+    public Mono<String> login(AuthRequest authRequest){
+        return userRepository.findByEmailAndPassword(authRequest.getEmail(), pbkdf2Encoder.encode(authRequest.getPassword()))
                 .map(user -> jwtUtil.generateToken(user));
 
     }
--- backend/src/test/java/com/team4/backend/service/UserServiceTest.java
+++ backend/src/test/java/com/team4/backend/service/UserServiceTest.java
@@ -1,6 +1,7 @@
 package com.team4.backend.service;
 
 import com.team4.backend.model.User;
+import com.team4.backend.model.dto.AuthRequest;
 import com.team4.backend.repository.UserRepository;
 import com.team4.backend.util.JwtUtil;
 import com.team4.backend.util.PBKDF2Encoder;
@@ -35,6 +36,7 @@ public class UserServiceTest {
     void login() {
         //ARRANGE
         User user1 = User.builder().registrationNumber("123456789").email("123456789@gmail.com").password("araa").build();
+        AuthRequest authRequest1 = new AuthRequest(user1.getEmail(),user1.getPassword());
         String token = "adasdadadsas";
 
         when(userRepository.findByEmailAndPassword(user1.getEmail(), pbkdf2Encoder.encode(user1.getPassword()))).thenReturn(Mono.just(user1));
@@ -42,12 +44,13 @@ public class UserServiceTest {
 
 
         User user2 = User.builder().registrationNumber("342432423").email("342432423@gmail.com").password("fdsfsd").build();
+        AuthRequest authRequest2 = new AuthRequest(user2.getEmail(),user2.getPassword());
 
         when(userRepository.findByEmailAndPassword(user2.getEmail(), pbkdf2Encoder.encode(user2.getPassword()))).thenReturn(Mono.empty());
 
         //ACT
-        Mono<String> returnedToken1 = userService.login(user1.getEmail(), user1.getPassword());
-        Mono<String> returnedToken2 = userService.login(user2.getEmail(), user2.getPassword());
+        Mono<String> returnedToken1 = userService.login(authRequest1);
+        Mono<String> returnedToken2 = userService.login(authRequest2);
 
         //ASSERT
         returnedToken1.subscribe(subToken -> assertEquals(token, subToken));
