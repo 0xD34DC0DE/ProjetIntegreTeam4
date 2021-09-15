0733e13a3aaa7a37dfe4af29205cd23bfd88f88b
William Jean Mireault <williamjeanmireault@gmail.com>
2021-09-15 14:11:07 -0400

    Fixed dependencies

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,7 +1,5 @@
 import './App.css';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
-import Login from './components/Login';
-import Register from './components/Register';
 import Home from './components/Home';
 
 function App() {
@@ -9,8 +7,6 @@ function App() {
     <Router>
       <div className="App"> 
         <Switch>
-          <Route path="/login" exact component={Login}/>
-          <Route path="/register" exact component={Register}/>
           <Route path="/home" exact component={Home}/>
         </Switch>
       </div>
--- frontend/src/components/Home.js
+++ frontend/src/components/Home.js
@@ -1,27 +1,10 @@
-import React, {useEffect, useState} from 'react';
-import { makeStyles } from '@material-ui/core';
-import axios from 'axios';
-
-const useStyles = makeStyles((theme) => ({
-}));
+import React, { useEffect, useState } from 'react';
+import { Typography } from '@mui/material';
 
 const Home = () => {
-    const classes = useStyles();
-    const [countries, setCountries] = useState([]);
-
-    useEffect(() => {
-        axios.get("https://restcountries.eu/rest/v2/all")
-        .then((response) => {
-            setCountries(response.data);
-        });
-    }, [])
-
     return (
         <div>
-            <h1>Home</h1>
-            <ul>
-                {countries.map((country, index) => <li key={index}>{country.name}</li>)}
-            </ul>
+            <Typography variant="h1">Home</Typography>
         </div>
     )
 }
