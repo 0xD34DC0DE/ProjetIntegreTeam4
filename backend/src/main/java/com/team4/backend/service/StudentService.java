d8197a68d85e393c48014a4a264fa9ab7b92d8e0
0xD34DC0DE <emerickpoulin31415@gmail.com>
2021-09-16 01:14:04 -0400

    added student service

---
+++ backend/src/main/java/com/team4/backend/service/StudentService.java
@@ -0,0 +1,22 @@
+package com.team4.backend.service;
+
+import com.team4.backend.model.Student;
+import com.team4.backend.repository.StudentRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import reactor.core.publisher.Mono;
+
+@Service
+public class StudentService {
+
+    private final StudentRepository studentRepository;
+
+    @Autowired
+    public StudentService(StudentRepository studentRepository) {
+        this.studentRepository = studentRepository;
+    }
+
+    public Mono<Student> registerStudent(Student student) {
+        return studentRepository.save(student);
+    }
+}
