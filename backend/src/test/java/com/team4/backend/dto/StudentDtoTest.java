bc1cc3ccef9b7086704d4c71e37ea31feeb490a9
0xD34DC0DE <emerickpoulin31415@gmail.com>
2021-09-16 01:12:41 -0400

    added student dto test

---
+++ backend/src/test/java/com/team4/backend/dto/StudentDtoTest.java
@@ -0,0 +1,10 @@
+package com.team4.backend.dto;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.test.context.ContextConfiguration;
+
+public class StudentDtoTest {
+
+//    @Test
+//    void convertStudentEntityToStudentDto_()
+}
