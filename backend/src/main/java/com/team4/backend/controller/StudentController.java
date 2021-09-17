6cbdf02d6ede4d65f84aa6251921c776efb94609
0xD34DC0DE <emerickpoulin31415@gmail.com>
2021-09-17 17:11:35 -0400

    put password encrypting inside service instead of controller

--- backend/src/main/java/com/team4/backend/controller/StudentController.java
+++ backend/src/main/java/com/team4/backend/controller/StudentController.java
@@ -3,13 +3,12 @@ package com.team4.backend.controller;
 
 import com.team4.backend.dto.StudentDto;
 import com.team4.backend.service.StudentService;
-import com.team4.backend.util.PBKDF2Encoder;
 import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.reactive.function.server.ServerResponse;
 import reactor.core.publisher.Mono;
 
 @RestController
@@ -18,11 +17,8 @@ public class StudentController {
 
     private final StudentService studentService;
 
-    private final PBKDF2Encoder pbkdf2Encoder;
-
-    public StudentController(StudentService studentService, PBKDF2Encoder pbkdf2Encoder) {
+    public StudentController(StudentService studentService) {
         this.studentService = studentService;
-        this.pbkdf2Encoder = pbkdf2Encoder;
     }
 
     @PostMapping("/register")
--- backend/src/main/java/com/team4/backend/service/StudentService.java
+++ backend/src/main/java/com/team4/backend/service/StudentService.java
@@ -1,7 +1,9 @@
 package com.team4.backend.service;
 
+import com.team4.backend.exception.DuplicateLoginException;
 import com.team4.backend.model.Student;
 import com.team4.backend.repository.StudentRepository;
+import com.team4.backend.util.PBKDF2Encoder;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import reactor.core.publisher.Mono;
@@ -11,12 +13,15 @@ public class StudentService {
 
     private final StudentRepository studentRepository;
 
-    @Autowired
-    public StudentService(StudentRepository studentRepository) {
+    private final PBKDF2Encoder pbkdf2Encoder;
+
+    public StudentService(StudentRepository studentRepository, PBKDF2Encoder pbkdf2Encoder) {
         this.studentRepository = studentRepository;
+        this.pbkdf2Encoder = pbkdf2Encoder;
     }
 
     public Mono<Student> registerStudent(Student student) {
+        student.setPassword(pbkdf2Encoder.encode(student.getPassword()));
         return studentRepository.save(student);
     }
 }
