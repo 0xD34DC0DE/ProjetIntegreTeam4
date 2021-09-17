cb43c7a08801ed2e21501fd407d3b2beaaee3114
0xD34DC0DE <emerickpoulin31415@gmail.com>
2021-09-16 23:55:10 -0400

    added password encoding to controller

--- backend/src/main/java/com/team4/backend/controller/StudentController.java
+++ backend/src/main/java/com/team4/backend/controller/StudentController.java
@@ -2,9 +2,8 @@ package com.team4.backend.controller;
 
 
 import com.team4.backend.dto.StudentDto;
-import com.team4.backend.mapper.StudentMapper;
 import com.team4.backend.service.StudentService;
-import org.springframework.beans.factory.annotation.Autowired;
+import com.team4.backend.util.PBKDF2Encoder;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -19,19 +18,18 @@ public class StudentController {
 
     private final StudentService studentService;
 
-    private final StudentMapper studentMapper;
+    private final PBKDF2Encoder pbkdf2Encoder;
 
-    @Autowired
-    public StudentController(StudentService studentService, StudentMapper studentMapper) {
+    public StudentController(StudentService studentService, PBKDF2Encoder pbkdf2Encoder) {
         this.studentService = studentService;
-        this.studentMapper = studentMapper;
+        this.pbkdf2Encoder = pbkdf2Encoder;
     }
 
     @PostMapping("/register")
     public Mono<ResponseEntity<Void>> register(@RequestBody StudentDto studentDto) {
-        ResponseEntity<Void> okResponse = new ResponseEntity<>(HttpStatus.OK);
-        return studentService.registerStudent(studentMapper.dtoToEntity(studentDto)).map(s -> okResponse);
+        ResponseEntity<Void> createdResponse = new ResponseEntity<>(HttpStatus.OK);
+        studentDto.setPassword(pbkdf2Encoder.encode(studentDto.getPassword()));
+        return studentService.registerStudent(StudentDto.dtoToEntity(studentDto)).map(s -> createdResponse);
     }
-
 
 }
