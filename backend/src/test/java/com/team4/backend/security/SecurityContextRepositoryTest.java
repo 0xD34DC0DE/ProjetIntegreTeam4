d0a788fea8da4e5364080eb4de20e7298d5ff318
Moussa <Moussa.Keita2411@gmail.com>
2021-09-17 15:51:39 -0400

    removing depencies not needed

--- backend/src/test/java/com/team4/backend/security/SecurityContextRepositoryTest.java
+++ backend/src/test/java/com/team4/backend/security/SecurityContextRepositoryTest.java
@@ -1,26 +1,11 @@
 package com.team4.backend.security;
 
 import com.team4.backend.util.JwtUtil;
-import org.apache.commons.collections4.multimap.ArrayListValuedHashMap;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
-import org.springframework.http.HttpHeaders;
-import org.springframework.http.HttpMethod;
-import org.springframework.http.HttpRequest;
-import org.springframework.mock.http.server.reactive.MockServerHttpRequest;
-import org.springframework.mock.web.server.MockServerWebExchange;
-import org.springframework.security.core.Authentication;
-import org.springframework.util.MultiValueMapAdapter;
-
-import javax.sound.sampled.Port;
-import java.net.InetAddress;
-import java.net.InetSocketAddress;
-import java.net.URI;
-import java.util.Collections;
-import java.util.HashMap;
 
 @ExtendWith(MockitoExtension.class)
 public class SecurityContextRepositoryTest {
@@ -34,6 +19,12 @@ public class SecurityContextRepositoryTest {
     @Test
     void load(){
         //ARRANGE
+        /*
+        MultiValueMap<String,String> multiValueMap =
+        MockServerWebExchange mockServerWebExchange = MockServerWebExchange.from(
+                MockServerHttpRequest.get("/test/test")
+                .headers()
+        );
 
         MockServerHttpRequest mockServerHttpRequest = new MockServerHttpRequest(
                 HttpMethod.GET,
@@ -44,10 +35,10 @@ public class SecurityContextRepositoryTest {
                 null,
                 null,
                 null,
-                null
-
-
+                dataBufferFlux
         );
+         */
+
 
         //ACT
 
