1f5f668686a4c4dd79a2c3b738ebdc9128332f45
0xD34DC0DE <emerickpoulin31415@gmail.com>
2021-09-16 23:55:40 -0400

    added student bidirectional dto to entity conversion

--- backend/src/main/java/com/team4/backend/dto/StudentDto.java
+++ backend/src/main/java/com/team4/backend/dto/StudentDto.java
@@ -1,5 +1,6 @@
 package com.team4.backend.dto;
 
+import com.team4.backend.model.Student;
 import com.team4.backend.model.enums.Role;
 import com.team4.backend.model.enums.StudentState;
 import lombok.*;
@@ -34,5 +35,34 @@ public class StudentDto extends UserDto {
         this.schoolName = schoolName;
         this.phoneNumber = phoneNumber;
         this.studentState = studentState;
+    }
+
+    public static Student dtoToEntity(StudentDto studentDto) {
+        return Student.studentBuilder()
+                .id(studentDto.getId())
+                .registrationNumber(studentDto.getRegistrationNumber())
+                .email(studentDto.getEmail())
+                .password(studentDto.getPassword())
+                .firstName(studentDto.getFirstName())
+                .lastName(studentDto.getLastName())
+                .registrationDate(studentDto.getRegistrationDate())
+                .schoolName(studentDto.getSchoolName())
+                .phoneNumber(studentDto.getPhoneNumber())
+                .studentState(studentDto.getStudentState())
+                .build();
+    }
+
+    public static StudentDto entityToDto(Student student) {
+        return StudentDto.builder()
+                .id(student.getId())
+                .registrationNumber(student.getRegistrationNumber())
+                .email(student.getEmail())
+                .firstName(student.getFirstName())
+                .lastName(student.getLastName())
+                .registrationDate(student.getRegistrationDate())
+                .schoolName(student.getSchoolName())
+                .phoneNumber(student.getPhoneNumber())
+                .studentState(student.getStudentState())
+                .build();
     }
 }
