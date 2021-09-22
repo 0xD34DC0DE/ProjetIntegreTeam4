7618badf7f85e0c662bb314d8156892222e2cafd
Moussa <Moussa.Keita2411@gmail.com>
2021-09-21 21:09:32 -0400

    addimg MonitorService --> findMonitorByEmail()

--- backend/src/main/java/com/team4/backend/service/MonitorService.java
+++ backend/src/main/java/com/team4/backend/service/MonitorService.java
@@ -1,13 +1,23 @@
 package com.team4.backend.service;
 
+import com.team4.backend.model.Monitor;
 import com.team4.backend.repository.MonitorRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
+import org.springframework.web.server.ResponseStatusException;
+import reactor.core.publisher.Mono;
 
 @Service
 public class MonitorService {
 
     @Autowired
     private MonitorRepository monitorRepository;
+
+
+    public Mono<Monitor> findMonitorByEmail(String email){
+        return monitorRepository.findByEmailAndIsEnabledTrue(email)
+                .switchIfEmpty(Mono.error(new ResponseStatusException(HttpStatus.NOT_FOUND,"Can't find monitor with this email!")));
+    }
 
 }
---
+++ backend/src/test/java/com/team4/backend/service/MonitorServiceTest.java
@@ -0,0 +1,47 @@
+package com.team4.backend.service;
+
+import com.team4.backend.model.Monitor;
+import com.team4.backend.repository.MonitorRepository;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.web.server.ResponseStatusException;
+import reactor.core.publisher.Mono;
+import reactor.test.StepVerifier;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.when;
+
+@ExtendWith(MockitoExtension.class)
+public class MonitorServiceTest {
+
+    @Mock
+    MonitorRepository monitorRepository;
+
+    @InjectMocks
+    MonitorService monitorService;
+
+    @Test
+    void findMonitorByEmail() {
+        //ARRANGE
+        String email1 = "marcM@desjardin.com";
+        Mono<Monitor> existingMonitor = Mono.just(Monitor.monitorBuilder().email(email1).build());
+        when(monitorRepository.findByEmailAndIsEnabledTrue(email1)).thenReturn(existingMonitor);
+
+        String email2 = "inexistantEmail@gmail.com";
+        Mono<Monitor> nonExistingMonitor = Mono.just(Monitor.monitorBuilder().email(email2).build());
+        when(monitorRepository.findByEmailAndIsEnabledTrue(email2)).thenReturn(Mono.empty());
+
+        //ACT
+        Mono<Monitor> returnedMonitor = monitorService.findMonitorByEmail(email1);
+        Mono<Monitor> noMonitorReturned = monitorService.findMonitorByEmail(email2);
+
+        //ASSERT
+        StepVerifier.create(existingMonitor)
+                .assertNext(monitor -> assertEquals(email1, monitor.getEmail())).verifyComplete();
+
+        //StepVerifier.create(nonExistingMonitor).verifyError(ResponseStatusException.class);
+    }
+}
