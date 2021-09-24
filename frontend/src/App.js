fb409a6e24368b9aaecc73be9f6f89dd38e534fc
Maxime Dupuis <dupuismaxime@hotmail.com>
2021-09-24 11:27:53 -0400

    Fixing PR OfferForm frontend

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-import { BrowserRouter, Route, Switch } from "react-router-dom";
+import { BrowserRouter, Route, Switch, Redirect } from "react-router-dom";
 import "./App.css";
 import StickyFooter from "./components/Footer";
 import Home from "./components/Home";
