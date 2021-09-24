8ced539268e42446f9b4a2882346c5b3ffc6b605
Maxime Dupuis <dupuismaxime@hotmail.com>
2021-09-24 09:15:11 -0400

    attempted validation on OfferForm

--- frontend/src/components/OfferForm.js
+++ frontend/src/components/OfferForm.js
@@ -14,15 +14,98 @@ import TextFormField from "./TextFormField";
 
 function OfferForm() {
   const emptyOffer = {
-    limitDateToApply: "",
-    beginningDate: "",
-    endingDate: "",
+    limitDateToApply: new Date(),
+    beginningDate: new Date(),
+    endingDate: new Date(),
     emailOfMonitor: "",
     companyName: "",
+    minSalary: 0,
+    maxSalary: 0,
     description: "",
   };
   const [offer, setOffer] = React.useState(emptyOffer);
   const [isValid, setIsValid] = React.useState(false);
+  const [errorMessage, setErrorMessage] = React.useState("");
+  const [errorIndexes, setErrorIndexes] = React.useState([]);
+  const [currentEvent, setCurrentEvent] = React.useState(null);
+  const [token, setToken] = React.useState(
+    "eyJhbGciOiJIUzUxMiJ9.eyJyb2xlIjoiSU5URVJOU0hJUF9NQU5BR0VSIiwic3ViIjoiZnJhbmNvaXNMYWNvdXJzaWVyZUBnbWFpbC5jb20iLCJpYXQiOjE2MzI0NDI0MzQsImV4cCI6MTYzMjQ3MTIzNH0.Cc8QW-_DV-zq6pnplPhCI9Re_iDrosZd2s6-SQ_wjBpIKB1ARk7Zsz05d8PLC9Ynm2aFsRzc4toN5CPMXAaKDg"
+  );
+
+  // Validation à faire
+  // TODO : limitDateToApply < beginningDate
+  // TODO : beginningDate < endingDate
+  // TODO : emailOfMonitor needs to have an email format
+  // TODO : minSalary < maxSalary
+  //
+
+  React.useEffect(() => {
+    const removeErrorIndex = (value) => {
+      for (var i = 0; i < errorIndexes.length; i++) {
+        if (errorIndexes[i] === value) {
+          var tmpErrorIndex = errorIndexes.splice(i, 1);
+          setErrorIndexes(tmpErrorIndex);
+          console.log("errorIndex", errorIndexes);
+        }
+      }
+    };
+    const validateOfferChange = () => {
+      let offerKeys = Object.keys(offer);
+      let offerValues = Object.values(offer);
+      switch (currentEvent != null && currentEvent.target.id) {
+        case offerKeys[1]:
+          setErrorMessage(
+            "La date limite pour appliquer ne peut venir après la date du début du stage."
+          );
+          setErrorIndexes(errorIndexes.concat(1));
+          break;
+        case offerKeys[2]:
+          setErrorMessage(
+            "La date du début du stage ne peut venir après la date de fin du stage."
+          );
+          setErrorIndexes(errorIndexes.concat(2));
+          break;
+        case offerKeys[3]: // Courriel du Moniteur
+          var err = emailValidation(offerValues[3]);
+          if (err) {
+            setErrorMessage(err);
+            if (!errorIndexes.includes(3))
+              setErrorIndexes(errorIndexes.concat(3));
+          } else {
+            removeErrorIndex(3);
+            setErrorMessage("");
+          }
+          break;
+        case offerKeys[4]:
+          break;
+        case offerKeys[5]:
+          if (offerValues[5] < 0) {
+            setErrorMessage("Le salaire minimum ne peut être négatif");
+            if (!errorIndexes.includes(5))
+              setErrorIndexes(errorIndexes.concat(5));
+          } else {
+            removeErrorIndex(5);
+            setErrorMessage("");
+          }
+          break;
+        case offerKeys[6]:
+          if (offerValues[6] < 0) {
+            setErrorMessage("Le salaire maximum ne peut être négatif");
+            if (!errorIndexes.includes(6))
+              setErrorIndexes(errorIndexes.concat(6));
+          } else {
+            removeErrorIndex(6);
+            setErrorMessage("");
+          }
+          break;
+        case offerKeys[7]:
+          break;
+        default:
+          break;
+      }
+    };
+    validateOfferChange();
+  }, [offer, currentEvent, errorIndexes]);
 
   const handleFormChange = (event) => {
     setOffer((previousForm) => ({
@@ -30,9 +113,22 @@ function OfferForm() {
       [event.target.id || event.target.name]: event.target.value,
     }));
 
+    setCurrentEvent(event);
+
     Object.values(offer).map((value, key, array) =>
       setIsValid(!array.includes("" || null))
     );
+  };
+
+  const emailValidation = (email) => {
+    if (
+      /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/.test(
+        email
+      )
+    ) {
+      return null;
+    }
+    return "Entrer une adresse courriel valide";
   };
 
   // CHANGE TOKEN AND LOGIN ON POSTMAN BEFORE TESTING
@@ -41,8 +137,7 @@ function OfferForm() {
       method: "POST",
       url: "http://localhost:8080/internshipOffer/addAnInternshipOffer",
       headers: {
-        Authorization:
-          "Bearer eyJhbGciOiJIUzUxMiJ9.eyJyb2xlIjoiSU5URVJOU0hJUF9NQU5BR0VSIiwic3ViIjoiZnJhbmNvaXNMYWNvdXJzaWVyZUBnbWFpbC5jb20iLCJpYXQiOjE2MzIzNjY4MzIsImV4cCI6MTYzMjM5NTYzMn0.3BJ6pGi_OTdq-W8LT9roTao-YfXptyvEWJR1tARuNju1k6X8232cdQEQQS-PnVU2LVm9UJnhGKE0fT3SSTa7WA",
+        Authorization: `Bearer ${token}`,
       },
       data: offer,
       responseType: "json",
@@ -56,6 +151,7 @@ function OfferForm() {
       });
   };
 
+  // SHOULD : Lorsque moniteur remplir le champ courriel par défaut
   return (
     <>
       <Dialog open={true}>
@@ -75,6 +171,14 @@ function OfferForm() {
                   value={Object.values(offer)[key]}
                   type={currentField.type}
                   visible={true}
+                  label={
+                    errorMessage && errorIndexes.includes(key)
+                      ? errorMessage
+                      : null
+                  }
+                  error={
+                    errorMessage && errorIndexes.includes(key) ? true : false
+                  }
                 />
               );
             })}
