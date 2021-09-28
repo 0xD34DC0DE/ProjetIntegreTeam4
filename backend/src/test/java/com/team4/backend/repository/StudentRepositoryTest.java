29c872ad2342933485709244e6e29a8c17b1afb9
0xD34DC0DE <emerickpoulin31415@gmail.com>
2021-09-28 14:00:33 -0400

    Removed tests of repository save

--- backend/src/test/java/com/team4/backend/repository/StudentRepositoryTest.java
+++ backend/src/test/java/com/team4/backend/repository/StudentRepositoryTest.java
@@ -1,7 +1,7 @@
 package com.team4.backend.repository;
 
 import com.team4.backend.model.Student;
-import com.team4.backend.model.enums.StudentState;
+import com.team4.backend.testdata.StudentMockData;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.TestInstance;
 import org.junit.jupiter.api.extension.ExtendWith;
@@ -25,50 +25,5 @@ public class StudentRepositoryTest {
     @Autowired
     StudentRepository studentRepository;
 
-    @Test
-    void CreateAccount() {
-        // TODO remove this test -> not testing "save" functionality of repository
-        final String registrationNumber = "123456789";
-        final String email = "123456789@gmail.com";
-        final String password = "passwd";
-        final String firstName = "John";
-        final String lastName = "Doe";
-        final String schoolName = "Example school";
-        final String phoneNumber = "123-456-7890";
-
-        final StudentState studentState = StudentState.REGISTERED;
-
-        Student student = Student.studentBuilder()
-                .registrationNumber(registrationNumber)
-                .email(email)
-                .password(password)
-                .firstName(firstName)
-                .lastName(lastName)
-                .schoolName(schoolName)
-                .phoneNumber(phoneNumber)
-                .studentState(studentState)
-                .build();
-
-        Mono<Student> studentMono = studentRepository.save(student);
-
-
-        StepVerifier
-                .create(studentMono)
-                .assertNext(s -> {
-                    s.setPassword("encrypted");
-                    assertNotNull(s.getId());
-                    assertEquals(s.getRegistrationNumber(), registrationNumber);
-                    assertEquals(s.getEmail(), email);
-                    assertNotEquals(s.getPassword(), password); // Password should be encrypted, not cleartext
-                    assertEquals(s.getFirstName(), firstName);
-                    assertEquals(s.getLastName(), lastName);
-                    assertEquals(s.getSchoolName(), schoolName);
-                    assertEquals(s.getPhoneNumber(), phoneNumber);
-                    assertEquals(s.getStudentState(), studentState);
-                })
-                .expectComplete()
-                .verify();
-        //TODO Add check for isEnable()
-    }
 
 }
