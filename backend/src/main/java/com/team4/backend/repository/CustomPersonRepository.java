87e138b1bb38618c99ca47e5284f255d70a943fe
0xD34DC0DE <emerickpoulin31415@gmail.com>
2021-09-28 17:33:58 -0400

    remove example code

--- backend/src/main/java/com/team4/backend/repository/CustomPersonRepository.java
+++
@@ -1,9 +0,0 @@
-package com.team4.backend.repository;
-
-import com.team4.backend.model.ExamplePerson;
-import reactor.core.publisher.Flux;
-
-
-public interface CustomPersonRepository {
-    Flux<ExamplePerson> findAllByNameFirstLetter(String letter);
-}
