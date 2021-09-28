12280b5b934ebaa9dec047b629ab330774076f11
0xD34DC0DE <emerickpoulin31415@gmail.com>
2021-09-28 17:40:29 -0400

    add missing lists to mock data && remove redundant mock data generator

--- backend/src/test/java/com/team4/backend/testdata/InternshipOfferMockData.java
+++ backend/src/test/java/com/team4/backend/testdata/InternshipOfferMockData.java
@@ -3,8 +3,11 @@ package com.team4.backend.testdata;
 import com.team4.backend.dto.InternshipOfferDto;
 import com.team4.backend.model.InternshipOffer;
 import com.team4.backend.model.Monitor;
+import com.team4.backend.model.Student;
 
 import java.time.LocalDate;
+import java.util.Arrays;
+import java.util.List;
 
 public abstract class InternshipOfferMockData {
 
@@ -19,6 +22,7 @@ public abstract class InternshipOfferMockData {
                 .companyName("desjardins")
                 .monitor(Monitor.monitorBuilder().email("rickJones@desjardins.com").build())
                 .description("Développeur Web")
+                .listInterestedStudents(getInterestedStudentsList())
                 .build();
     }
 
@@ -33,24 +37,19 @@ public abstract class InternshipOfferMockData {
                 .companyName("desjardins")
                 .emailOfMonitor("rickJones@desjardins.com")
                 .description("Développeur Web")
+                .listEmailInterestedStudents(getInterestedStudentsEmailList())
                 .build();
-
-        return internshipOfferDto;
     }
 
-    public static InternshipOfferDto getInternshipOfferDtoWithNonExistentMonitor() {
-        final InternshipOfferDto internshipOfferDto =  InternshipOfferDto.builder()
-                .id("das0924kdads")
-                .limitDateToApply(LocalDate.now().plusMonths(1))
-                .beginningDate(LocalDate.now().plusMonths(2))
-                .endingDate(LocalDate.now().plusMonths(6))
-                .minSalary(22.5f)
-                .maxSalary(23.5f)
-                .companyName("desjardins")
-                .emailOfMonitor("noneExistent@desjardins.com")
-                .description("Développeur Web")
-                .build();
-
-        return internshipOfferDto;
+    public static List<String> getInterestedStudentsEmailList() {
+        return Arrays.asList("student1@email.com", "student2@email.com");
     }
+
+    public static List<Student> getInterestedStudentsList() {
+        return Arrays.asList(
+                Student.studentBuilder().email("student1@email.com").build(),
+                Student.studentBuilder().email("student2@email.com").build()
+        );
+    }
+
 }
