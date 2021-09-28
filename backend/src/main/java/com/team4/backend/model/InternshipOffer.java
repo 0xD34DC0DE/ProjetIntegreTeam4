55a8c044133b17d5ffa48aa3f56edb65cbe1f861
0xD34DC0DE <emerickpoulin31415@gmail.com>
2021-09-28 13:00:26 -0400

    changed dto constructor to normal constructor

--- backend/src/main/java/com/team4/backend/model/InternshipOffer.java
+++ backend/src/main/java/com/team4/backend/model/InternshipOffer.java
@@ -6,6 +6,7 @@ import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.DBRef;
 import org.springframework.data.mongodb.core.mapping.Document;
 
 import java.io.Serializable;
@@ -28,26 +29,48 @@ public class InternshipOffer implements Serializable {
 
     private LocalDate endingDate;
 
+    @DBRef
     private Monitor monitor;
 
-    private String companyName, description;
+    private String companyName;
 
-    private Float minSalary, maxSalary;
+    private String description;
 
-    private boolean isValidated, isExclusive;
+    private Float minSalary;
 
+    private Float maxSalary;
+
+    private boolean isValidated;
+
+    private boolean isExclusive;
+
+    @DBRef
     private List<Student> listInterestedStudents;
 
+    @DBRef
     private List<Student> listExclusiveStudents;
 
-    public InternshipOffer(InternshipOfferDto internshipOfferDTO, Monitor monitor) {
-        this.limitDateToApply = internshipOfferDTO.getLimitDateToApply();
-        this.beginningDate = internshipOfferDTO.getBeginningDate();
-        this.endingDate = internshipOfferDTO.getEndingDate();
-        this.companyName = internshipOfferDTO.getCompanyName();
-        this.description = internshipOfferDTO.getDescription();
-        this.minSalary = internshipOfferDTO.getMinSalary();
-        this.maxSalary = internshipOfferDTO.getMaxSalary();
+    public InternshipOffer(String id,
+                           LocalDate limitDateToApply,
+                           LocalDate beginningDate,
+                           LocalDate endingDate,
+                           Monitor monitor,
+                           String companyName,
+                           String description,
+                           Float minSalary,
+                           Float maxSalary,
+                           boolean isValidated,
+                           boolean isExclusive) {
+        this.id = id;
+        this.limitDateToApply = limitDateToApply;
+        this.beginningDate = beginningDate;
+        this.endingDate = endingDate;
         this.monitor = monitor;
+        this.companyName = companyName;
+        this.description = description;
+        this.minSalary = minSalary;
+        this.maxSalary = maxSalary;
+        this.isValidated = isValidated;
+        this.isExclusive = isExclusive;
     }
 }
