d409dd6de14568d985d594f8f51c6627605efa3d
0xD34DC0DE <emerickpoulin31415@gmail.com>
2021-10-02 01:43:03 -0400

    EQ4-3: updated to map new view dto's

--- backend/src/main/java/com/team4/backend/mapping/InternshipOfferMapper.java
+++ backend/src/main/java/com/team4/backend/mapping/InternshipOfferMapper.java
@@ -1,41 +1,56 @@
 package com.team4.backend.mapping;
 
-import com.team4.backend.dto.InternshipOfferDto;
+import com.team4.backend.dto.InternshipOfferCreationDto;
+import com.team4.backend.dto.InternshipOfferMonitorViewDto;
+import com.team4.backend.dto.InternshipOfferStudentViewDto;
 import com.team4.backend.model.InternshipOffer;
 
 import java.util.Collections;
 
 public abstract class InternshipOfferMapper {
 
-    public static InternshipOffer toEntity(InternshipOfferDto internshipOfferDto) {
+    public static InternshipOffer toEntity(InternshipOfferCreationDto internshipOfferCreationDto) {
         return InternshipOffer.builder()
-                .limitDateToApply(internshipOfferDto.getLimitDateToApply())
-                .beginningDate(internshipOfferDto.getBeginningDate())
-                .endingDate(internshipOfferDto.getEndingDate())
-                .companyName(internshipOfferDto.getCompanyName())
-                .emailOfMonitor(internshipOfferDto.getEmailOfMonitor())
-                .description(internshipOfferDto.getDescription())
-                .minSalary(internshipOfferDto.getMinSalary())
-                .maxSalary(internshipOfferDto.getMaxSalary())
+                .limitDateToApply(internshipOfferCreationDto.getLimitDateToApply())
+                .beginningDate(internshipOfferCreationDto.getBeginningDate())
+                .endingDate(internshipOfferCreationDto.getEndingDate())
+                .companyName(internshipOfferCreationDto.getCompanyName())
+                .emailOfMonitor(internshipOfferCreationDto.getEmailOfMonitor())
+                .description(internshipOfferCreationDto.getDescription())
+                .minSalary(internshipOfferCreationDto.getMinSalary())
+                .maxSalary(internshipOfferCreationDto.getMaxSalary())
                 .listEmailInterestedStudents(Collections.emptyList())
                 .isValidated(false)
                 .isExclusive(false)
                 .build();
     }
 
-    public static InternshipOfferDto toDto(InternshipOffer internshipOffer) {
-        return InternshipOfferDto.builder()
+    public static InternshipOfferStudentViewDto toStudentViewDto(InternshipOffer internshipOffer) {
+        return InternshipOfferStudentViewDto.builder()
                 .id(internshipOffer.getId())
                 .limitDateToApply(internshipOffer.getLimitDateToApply())
                 .beginningDate(internshipOffer.getBeginningDate())
                 .endingDate(internshipOffer.getEndingDate())
                 .companyName(internshipOffer.getCompanyName())
-                .emailOfMonitor(internshipOffer.getEmailOfMonitor())
+                .description(internshipOffer.getDescription())
+                .minSalary(internshipOffer.getMinSalary())
+                .maxSalary(internshipOffer.getMaxSalary())
+                .isExclusive(internshipOffer.getIsExclusive())
+                .build();
+    }
+
+    public static InternshipOfferMonitorViewDto toMonitorView(InternshipOffer internshipOffer) {
+        return InternshipOfferMonitorViewDto.builder()
+                .id(internshipOffer.getId())
+                .limitDateToApply(internshipOffer.getLimitDateToApply())
+                .beginningDate(internshipOffer.getBeginningDate())
+                .endingDate(internshipOffer.getEndingDate())
+                .companyName(internshipOffer.getCompanyName())
                 .description(internshipOffer.getDescription())
                 .minSalary(internshipOffer.getMinSalary())
                 .maxSalary(internshipOffer.getMaxSalary())
                 .listEmailInterestedStudents(internshipOffer.getListEmailInterestedStudents())
-                .isValidated(internshipOffer.isValidated()).build();
+                .build();
     }
 
 }
