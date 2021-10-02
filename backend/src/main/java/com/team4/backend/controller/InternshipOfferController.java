40286ac911e2d60ca59e9b1d0fad8e4b4d042472
0xD34DC0DE <emerickpoulin31415@gmail.com>
2021-10-02 01:41:27 -0400

    EQ4-3: added internship offers student view endpoint

--- backend/src/main/java/com/team4/backend/controller/InternshipOfferController.java
+++ backend/src/main/java/com/team4/backend/controller/InternshipOfferController.java
@@ -1,15 +1,18 @@
 package com.team4.backend.controller;
 
-import com.team4.backend.dto.InternshipOfferDto;
+import com.team4.backend.dto.InternshipOfferCreationDto;
+import com.team4.backend.dto.InternshipOfferStudentViewDto;
+import com.team4.backend.exception.UserNotFoundException;
+import com.team4.backend.mapping.InternshipOfferMapper;
 import com.team4.backend.service.InternshipOfferService;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.server.ResponseStatusException;
 import reactor.core.publisher.Mono;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/internshipOffer")
@@ -23,10 +26,23 @@ public class InternshipOfferController {
 
     @PostMapping("/addAnInternshipOffer")
     @PreAuthorize("hasAnyAuthority('INTERNSHIP_MANAGER','MONITOR')")
-    public Mono<ResponseEntity<String>> addAnInternshipOffer(@RequestBody InternshipOfferDto internshipOfferDTO) {
-        return internshipOfferService.addAnInternshipOffer(internshipOfferDTO)
+    public Mono<ResponseEntity<String>> addAnInternshipOffer(
+            @RequestBody InternshipOfferCreationDto internshipOfferCreationDto) {
+        return internshipOfferService.addAnInternshipOffer(internshipOfferCreationDto)
                 .flatMap(internshipOffer -> Mono.just(ResponseEntity.status(HttpStatus.CREATED).body("")))
-                .onErrorResume(error -> Mono.just(ResponseEntity.status(HttpStatus.NOT_FOUND).body(error.getMessage())));
+                .onErrorResume(error ->
+                        Mono.just(ResponseEntity.status(HttpStatus.NOT_FOUND).body(error.getMessage())));
+    }
+
+    @GetMapping(value = "/studentInternshipOffers/{email}")
+    @PreAuthorize("hasAuthority('STUDENT')")
+    public Mono<ResponseEntity<List<InternshipOfferStudentViewDto>>> studentInternshipOffers(@PathVariable("email") String studentEmail) {
+        return internshipOfferService.getStudentInternshipOffers(studentEmail)
+                .map(InternshipOfferMapper::toStudentViewDto)
+                .onErrorMap(
+                        UserNotFoundException.class,
+                        e -> new ResponseStatusException(HttpStatus.NOT_FOUND, e.getMessage())
+                ).collectList().flatMap(offers -> Mono.just(ResponseEntity.status(HttpStatus.OK).body(offers)));
     }
 
 }
