2a088648a4a265d8fcf5e9bcd2b675e325846d72
0xD34DC0DE <emerickpoulin31415@gmail.com>
2021-10-04 21:20:57 -0400

    EQ4-3: better look

--- frontend/src/components/OfferView.js
+++ frontend/src/components/OfferView.js
@@ -1,17 +1,19 @@
-import { Container, Typography } from '@mui/material'
+import { Container, Paper, Typography } from '@mui/material'
 import React from 'react'
 
 function OfferView({ companyName, beginningDate, endingDate, limitDateToApply, minSalary, maxSalary, description }) {
   return (
     <>
-      <Container>
-        <Typography>Compagnie: {companyName}</Typography>
-        <Typography>Début: {beginningDate}</Typography>
-        <Typography>Fin: {endingDate}</Typography>
-        <Typography>Date limite d'application: {limitDateToApply}</Typography>
-        <Typography>Taux horaire: {minSalary}$-{maxSalary}</Typography>
-        <Typography>Description:</Typography>
-        <Typography>{description}</Typography>
+      <Container mb={3}>
+        <Paper elevation={3}>
+          <Typography variant="title">Compagnie: {companyName}</Typography>
+          <Typography>Début: {beginningDate}</Typography>
+          <Typography>Fin: {endingDate}</Typography>
+          <Typography>Date limite d'application: {limitDateToApply}</Typography>
+          <Typography>Taux horaire: {minSalary}$ - {maxSalary}$</Typography>
+          <Typography>Description:</Typography>
+          <Typography>{description}</Typography>
+        </Paper>
       </Container>
     </>
   )
