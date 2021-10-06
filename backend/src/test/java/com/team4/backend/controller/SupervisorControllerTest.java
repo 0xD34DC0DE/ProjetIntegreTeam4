966a22277fae08998e31e0f161880243f6c099f1
William Jean Mireault <williamjeanmireault@gmail.com>
2021-10-06 14:29:37 -0400

    EQ4-14 - Implemented basic structure for supervisor's tests

--- backend/src/test/java/com/team4/backend/controller/SupervisorControllerTest.java
+++ backend/src/test/java/com/team4/backend/controller/SupervisorControllerTest.java
@@ -1,4 +1,28 @@
 package com.team4.backend.controller;
 
+import com.team4.backend.service.SupervisorService;
+import com.team4.backend.service.UserService;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.security.reactive.ReactiveSecurityAutoConfiguration;
+import org.springframework.boot.test.autoconfigure.web.reactive.WebFluxTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+import org.springframework.test.web.reactive.server.WebTestClient;
+
+@EnableAutoConfiguration
+@ExtendWith(SpringExtension.class)
+@WebFluxTest(value = SupervisorController.class, excludeAutoConfiguration = ReactiveSecurityAutoConfiguration.class)
 public class SupervisorControllerTest {
+
+    @Autowired
+    private WebTestClient webTestClient;
+
+    @MockBean
+    SupervisorService supervisorService;
+
+    @MockBean
+    UserService userService;
+
 }
--- backend/src/test/java/com/team4/backend/service/SupervisorServiceTest.java
+++ backend/src/test/java/com/team4/backend/service/SupervisorServiceTest.java
@@ -1,4 +1,25 @@
 package com.team4.backend.service;
 
+import com.team4.backend.repository.SupervisorRepository;
+import com.team4.backend.util.PBKDF2Encoder;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+@ExtendWith(MockitoExtension.class)
 public class SupervisorServiceTest {
+
+    @Mock
+    SupervisorRepository supervisorRepository;
+
+    @Mock
+    PBKDF2Encoder pbkdf2Encoder;
+
+    @Mock
+    UserService userService;
+
+    @InjectMocks
+    SupervisorService supervisorService;
+
 }
