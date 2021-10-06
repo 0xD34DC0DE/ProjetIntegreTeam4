b05bde9ba96bd14d3c70301173f5c336f26a4aa4
William Jean Mireault <williamjeanmireault@gmail.com>
2021-10-06 14:15:01 -0400

    EQ4-14 - Implemented supervisor dto

--- backend/src/main/java/com/team4/backend/dto/SupervisorDto.java
+++ backend/src/main/java/com/team4/backend/dto/SupervisorDto.java
@@ -1,4 +1,39 @@
 package com.team4.backend.dto;
 
-public class SupervisorDto {
+import com.team4.backend.model.Student;
+import com.team4.backend.model.enums.Role;
+import lombok.Builder;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.NoArgsConstructor;
+
+import java.io.Serializable;
+import java.time.LocalDate;
+import java.util.List;
+
+@Data
+@NoArgsConstructor
+@EqualsAndHashCode(callSuper = true)
+public class SupervisorDto extends UserDto implements Serializable {
+
+    private List<Student> students;
+
+    @Builder
+    public SupervisorDto(String id,
+                         String email,
+                         String password,
+                         String firstName,
+                         String lastName,
+                         LocalDate registrationDate,
+                         String phoneNumber) {
+        super(id,
+                email,
+                password,
+                firstName,
+                lastName,
+                registrationDate,
+                phoneNumber,
+                Role.SUPERVISOR);
+    }
+
 }
