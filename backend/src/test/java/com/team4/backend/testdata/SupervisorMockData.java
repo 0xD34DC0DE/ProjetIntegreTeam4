91c340a553b4e37d2b4606215d42354956d00907
William Jean Mireault <williamjeanmireault@gmail.com>
2021-10-06 14:34:30 -0400

    EQ4-14 - Added mock data for supervisor

--- backend/src/test/java/com/team4/backend/testdata/SupervisorMockData.java
+++ backend/src/test/java/com/team4/backend/testdata/SupervisorMockData.java
@@ -1,4 +1,34 @@
 package com.team4.backend.testdata;
 
+import com.team4.backend.dto.SupervisorDto;
+import com.team4.backend.model.Supervisor;
+
 public class SupervisorMockData {
+
+    public static Supervisor getMockSupervisor() {
+        return Supervisor.supervisorBuilder()
+                .id("615a32ce577ae63d7b159b17")
+                .email("jonathan_22@outlook.com")
+                .password("soleil31@")
+                .firstName("Jonathan")
+                .lastName("Poulin")
+                .students(null)
+                .phoneNumber("438-999-1234")
+                .registrationDate(null) // Current date
+                .build();
+    }
+
+    public static SupervisorDto getMockSupervisorDto() {
+        return SupervisorDto.builder()
+                .id("615a32ce577ae63d7b159b17")
+                .email("jonathan_22@outlook.com")
+                .password("soleil31@")
+                .firstName("Jonathan")
+                .lastName("Poulin")
+                .students(null)
+                .phoneNumber("438-999-1234")
+                .registrationDate(null) // Current date
+                .build();
+    }
+
 }
