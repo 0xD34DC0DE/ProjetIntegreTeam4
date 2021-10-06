6f3f19615976506f965c5dfb66296d5a75de6b39
William Jean Mireault <williamjeanmireault@gmail.com>
2021-10-06 14:43:44 -0400

    EQ4-14 - Implemented mapper's tests for supervisor

--- backend/src/test/java/com/team4/backend/mapping/SupervisorMapperTest.java
+++ backend/src/test/java/com/team4/backend/mapping/SupervisorMapperTest.java
@@ -1,4 +1,52 @@
 package com.team4.backend.mapping;
 
+import com.team4.backend.dto.SupervisorDto;
+import com.team4.backend.model.Supervisor;
+import com.team4.backend.testdata.SupervisorMockData;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNull;
+
 public class SupervisorMapperTest {
+
+    @Test
+    void mapDtoToEntity() {
+        //ARANGE
+        SupervisorDto dto = SupervisorMockData.getMockSupervisorDto();
+
+        //ACT
+        Supervisor entity = SupervisorMapper.toEntity(dto);
+
+        //ASSERT
+        assertNull(entity.getId());
+        assertEquals(dto.getEmail(), entity.getEmail());
+        assertEquals(dto.getPassword(), entity.getPassword());
+        assertEquals(dto.getFirstName(), entity.getFirstName());
+        assertEquals(dto.getLastName(), entity.getLastName());
+        assertEquals(dto.getPhoneNumber(), entity.getPhoneNumber());
+        assertEquals(dto.getStudents(), entity.getStudents());
+        assertEquals(dto.getRegistrationDate(), entity.getRegistrationDate());
+    }
+
+    @Test
+    void mapEntityToDto() {
+        //ARANGE
+        Supervisor entity = SupervisorMockData.getMockSupervisor();
+
+        //ACT
+        SupervisorDto dto = SupervisorMapper.toDto(entity);
+
+        //ASSERT
+        assertNull(dto.getPassword()); // password shouldn't be given to frontend
+
+        assertEquals(entity.getId(), dto.getId());
+        assertEquals(entity.getEmail(), dto.getEmail());
+        assertEquals(entity.getFirstName(), dto.getFirstName());
+        assertEquals(entity.getLastName(), dto.getLastName());
+        assertEquals(entity.getPhoneNumber(), dto.getPhoneNumber());
+        assertEquals(entity.getStudents(), dto.getStudents());
+        assertEquals(entity.getRegistrationDate(), dto.getRegistrationDate());
+    }
+
 }
