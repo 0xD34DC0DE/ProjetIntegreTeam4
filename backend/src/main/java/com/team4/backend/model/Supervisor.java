21ebdc9686e9b32f60dcbfec6d7ebdd5fb57b2f4
William Jean Mireault <williamjeanmireault@gmail.com>
2021-10-06 14:12:02 -0400

    EQ4-14 - Implemented supervisor model

--- backend/src/main/java/com/team4/backend/model/Supervisor.java
+++ backend/src/main/java/com/team4/backend/model/Supervisor.java
@@ -1,4 +1,39 @@
 package com.team4.backend.model;
 
-public class Supervisor {
+import com.team4.backend.model.enums.Role;
+import lombok.*;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+import java.io.Serializable;
+import java.time.LocalDate;
+import java.util.List;
+
+@Data
+@ToString
+@NoArgsConstructor
+@Document("users")
+@EqualsAndHashCode(callSuper = true)
+public class Supervisor extends User implements Serializable {
+
+    private List<Student> students;
+
+    @Builder(builderMethodName = "supervisorBuilder")
+    public Supervisor(String id,
+                      String email,
+                      String firstName,
+                      String lastName,
+                      String password,
+                      String phoneNumber,
+                      LocalDate registrationDate) {
+        super(id,
+                email,
+                firstName,
+                lastName,
+                password,
+                phoneNumber,
+                Role.SUPERVISOR,
+                true,
+                registrationDate);
+    }
+
 }
