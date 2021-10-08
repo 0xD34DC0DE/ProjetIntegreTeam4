bee3bed2e764168c7a04a40bfff8ca549891f37a
0xD34DC0DE <emerickpoulin31415@gmail.com>
2021-10-07 20:16:59 -0400

    EQ4-3: move business logic to service

--- backend/src/main/java/com/team4/backend/controller/InternshipOfferController.java
+++ backend/src/main/java/com/team4/backend/controller/InternshipOfferController.java
@@ -5,21 +5,17 @@ import com.team4.backend.dto.InternshipOfferStudentViewDto;
 import com.team4.backend.exception.UserNotFoundException;
 import com.team4.backend.mapping.InternshipOfferMapper;
 import com.team4.backend.service.InternshipOfferService;
-import org.springframework.data.domain.PageRequest;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
-import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.server.ResponseStatusException;
 import reactor.core.publisher.Mono;
 
 import javax.validation.ConstraintViolationException;
-import javax.validation.constraints.Min;
 import java.util.List;
 
 @RestController
-@Validated
 @RequestMapping("/internshipOffer")
 public class InternshipOfferController {
 
@@ -51,8 +47,8 @@ public class InternshipOfferController {
     public Mono<ResponseEntity<List<InternshipOfferStudentViewDto>>> studentExclusiveInternshipOffers(
             @PathVariable("email") String studentEmail,
             @RequestParam(value = "page", defaultValue = "0") Integer page,
-            @RequestParam(value = "size", defaultValue = "5") @Min(1) Integer size) {
-        return internshipOfferService.getStudentExclusiveOffers(studentEmail, PageRequest.of(page, size))
+            @RequestParam(value = "size", defaultValue = "5") Integer size) {
+        return internshipOfferService.getStudentExclusiveOffers(studentEmail, page, size)
                 .onErrorMap(
                         UserNotFoundException.class,
                         e -> new ResponseStatusException(HttpStatus.NOT_FOUND, e.getMessage())
@@ -65,14 +61,10 @@ public class InternshipOfferController {
     @GetMapping(value = "/studentInternshipOffers")
     @PreAuthorize("hasAuthority('STUDENT')")
     public Mono<ResponseEntity<List<InternshipOfferStudentViewDto>>> studentGeneralInternshipOffers(
-            @Min(0) @RequestParam(value = "page", defaultValue = "0") Integer page,
-            @Min(1) @RequestParam(value = "size", defaultValue = "5") Integer size) {
-        return internshipOfferService.getGeneralInternshipOffers(PageRequest.of(page, size))
+            @RequestParam(value = "page", defaultValue = "0") Integer page,
+            @RequestParam(value = "size", defaultValue = "5") Integer size) {
+        return internshipOfferService.getGeneralInternshipOffers(page, size)
                 .map(InternshipOfferMapper::toStudentViewDto)
-                .onErrorMap(
-                        UserNotFoundException.class,
-                        e -> new ResponseStatusException(HttpStatus.NOT_FOUND, e.getMessage())
-                )
                 .collectList()
                 .map(ResponseEntity::ok);
     }
@@ -80,8 +72,8 @@ public class InternshipOfferController {
     @GetMapping(value = {"/pageCount/{email}"})
     @PreAuthorize("hasAuthority('STUDENT')")
     public Mono<ResponseEntity<Long>> getInternshipOffersCount(
-            @PathVariable(value = "email", required = false) String studentEmail,
-            @Min(1) @RequestParam(value = "size", defaultValue = "5") Integer size) {
+            @PathVariable("email") String studentEmail,
+            @RequestParam(value = "size", defaultValue = "5") Integer size) {
         return internshipOfferService.getInternshipOffersPageCount(studentEmail, size)
                 .onErrorMap(
                         UserNotFoundException.class,
@@ -93,12 +85,8 @@ public class InternshipOfferController {
     @GetMapping(value = "/pageCount")
     @PreAuthorize("hasAuthority('STUDENT')")
     public Mono<ResponseEntity<Long>> getInternshipOffersCount(
-            @RequestParam(value = "size", defaultValue = "5") int size) {
-        return internshipOfferService.getInternshipOffersPageCount(null, size)
-                .onErrorMap(
-                        UserNotFoundException.class,
-                        e -> new ResponseStatusException(HttpStatus.NOT_FOUND, e.getMessage())
-                )
+            @RequestParam(value = "size", defaultValue = "5") Integer size) {
+        return internshipOfferService.getInternshipOffersPageCount(size)
                 .map(ResponseEntity::ok);
     }
 
