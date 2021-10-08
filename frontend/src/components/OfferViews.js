350874d351efe0ce83da83c8f900878bcc3b066f
0xD34DC0DE <emerickpoulin31415@gmail.com>
2021-10-08 01:43:25 -0400

    EQ4-3: Add request cancellation

--- frontend/src/components/OfferViews.js
+++ frontend/src/components/OfferViews.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect, useContext } from 'react'
+import React, { useState, useEffect, useContext, useRef } from 'react'
 import { CircularProgress, Stack, Typography, Box, Tab, Tabs, Pagination } from '@mui/material'
 import OfferView from './OfferView';
 import axios from "axios";
@@ -12,11 +12,23 @@ function OfferViews() {
   const [pagingCount, setPagingCount] = useState(1);
   const [currentPage, setCurrentPage] = useState(1);
 
+  const CancelTokenOffers = axios.CancelToken;
+  let cancelOffers = useRef(null);
+
+  const CancelTokenPageCount = axios.CancelToken;
+  let cancelPageCount = useRef(null);
+
   useEffect(() => {
     if (userInfo.loggedIn) {
       const getInternshipOffersPagingCount = () => {
+
         setPagingCount(1);
+        
         const email = (currentTab === 1) ? "/" + userInfo.email : "";
+
+        if(cancelPageCount.current) {
+          cancelPageCount.current();
+        }
         axios({
           method: "GET",
           url: `http://localhost:8080/internshipOffer/pageCount${email}?size=${offersPerPage}`,
@@ -24,24 +36,36 @@ function OfferViews() {
             Authorization: userInfo.jwt,
           },
           responseType: "json",
+          cancelToken: new CancelTokenPageCount((c) => {
+            cancelPageCount.current = c;
+          })
         })
           .then((response) => {
             setPagingCount(response.data);
           })
           .catch((error) => {
-            console.error(error);
+            if (!axios.isCancel(error)) {
+              console.error(error);
+            }
           });
       }
       getInternshipOffersPagingCount();
     }
-  }, [currentTab, currentPage, userInfo]);
+  }, [currentTab, currentPage, userInfo, CancelTokenPageCount]);
 
   useEffect(() => {
     if (userInfo.loggedIn) {
       const getInternshipOffers = () => {
+        
         setInternshipOffers(null);
+
         const email = (currentTab === 1) ? "/" + userInfo.email : "";
-        const endpoint = `http://localhost:8080/internshipOffer/studentInternshipOffers${email}?page=${currentPage - 1}&size=${offersPerPage}`
+        const endpoint = `http://localhost:8080/internshipOffer/studentInternshipOffers${email}?page=${currentPage - 1}&size=${offersPerPage}`;
+
+        if(cancelOffers.current) {
+          cancelOffers.current();
+        }
+
         axios({
           method: "GET",
           url: endpoint,
@@ -49,20 +73,25 @@ function OfferViews() {
             Authorization: userInfo.jwt,
           },
           responseType: "json",
+          cancelToken: new CancelTokenOffers((c) => {
+            cancelOffers.current = c;
+          })
         })
           .then((response) => {
-            console.log(response);
             setInternshipOffers(response.data);
           })
           .catch((error) => {
-            console.error(error);
+            if (!axios.isCancel(error)) {
+              console.error(error);
+            }
           });
       };
       getInternshipOffers();
     }
-  }, [currentTab, currentPage, userInfo]);
+  }, [currentTab, currentPage, userInfo, CancelTokenOffers]);
 
   const handleTabChange = (event, selectedTab) => {
+    setCurrentPage(1);
     setCurrentTab(selectedTab);
   };
 
@@ -74,7 +103,7 @@ function OfferViews() {
     <>
       <Box sx={{ borderBottom: 1, borderColor: 'divider', display: 'flex' }}>
         <Tabs sx={{ width: '100%' }} value={currentTab} onChange={handleTabChange} aria-label="Affichage des offres" centered>
-          <Tab label="Offres générales" index={0} sx={{ width: '50%' }}/>
+          <Tab label="Offres générales" index={0} sx={{ width: '50%' }} />
           <Tab label="Offres exclusives" index={1} sx={{ width: '50%' }} />
         </Tabs>
       </Box>
@@ -89,7 +118,7 @@ function OfferViews() {
               internshipOffers.map((offer, i) => <OfferView key={i} {...offer} />)
         }
         {
-            internshipOffers != null && <Pagination sx={{mt: 5}} count={pagingCount} page={currentPage} onChange={handlePageChange} />
+          internshipOffers != null && <Pagination sx={{ mt: 5 }} count={pagingCount} page={currentPage} onChange={handlePageChange} />
         }
       </Stack>
     </>
