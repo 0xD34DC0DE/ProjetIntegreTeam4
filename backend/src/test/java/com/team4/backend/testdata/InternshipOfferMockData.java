1a790b8202cb270371e7c006b1c88f1f0b8f9bac
0xD34DC0DE <emerickpoulin31415@gmail.com>
2021-10-07 20:18:31 -0400

    EQ4-3: added internshipOffer list mock data

--- backend/src/test/java/com/team4/backend/testdata/InternshipOfferMockData.java
+++ backend/src/test/java/com/team4/backend/testdata/InternshipOfferMockData.java
@@ -1,9 +1,11 @@
 package com.team4.backend.testdata;
 
 import com.team4.backend.dto.InternshipOfferCreationDto;
+import com.team4.backend.dto.InternshipOfferStudentViewDto;
 import com.team4.backend.model.InternshipOffer;
 
 import java.time.LocalDate;
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
@@ -37,8 +39,49 @@ public abstract class InternshipOfferMockData {
                 .build();
     }
 
+    public static InternshipOfferStudentViewDto getInternshipStudentViewDto() {
+        return InternshipOfferStudentViewDto.builder()
+                .id("id")
+                .limitDateToApply(LocalDate.now().plusMonths(1))
+                .beginningDate(LocalDate.now().plusMonths(2))
+                .endingDate(LocalDate.now().plusMonths(6))
+                .minSalary(22.5f)
+                .maxSalary(23.5f)
+                .companyName("desjardins")
+                .description("Développeur Web")
+                .build();
+    }
+
     public static List<String> getInterestedStudentsEmailList() {
         return Arrays.asList("student1@email.com", "student2@email.com");
+    }
+
+    public static List<InternshipOffer> getListInternshipOffer(int count) {
+        List<InternshipOffer> internshipOffers = new ArrayList<>();
+
+        for (int i = 0; i < count; i++) {
+            InternshipOffer internshipOffer = getInternshipOffer();
+
+            internshipOffer.setId(internshipOffer.getId() + "_" + i);
+
+            internshipOffers.add(internshipOffer);
+        }
+
+        return internshipOffers;
+    }
+
+    public static List<InternshipOfferStudentViewDto> getListInternshipOfferStudentViewDto(int count) {
+        List<InternshipOfferStudentViewDto> internshipOffers = new ArrayList<>();
+
+        for (int i = 0; i < count; i++) {
+            InternshipOfferStudentViewDto internshipOfferStudentViewDto = getInternshipStudentViewDto();
+
+            internshipOfferStudentViewDto.setId(internshipOfferStudentViewDto.getId() + "_" + i);
+
+            internshipOffers.add(internshipOfferStudentViewDto);
+        }
+
+        return internshipOffers;
     }
     
 }
