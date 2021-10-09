b14ff21e7a2b68bbdcb06e2f416fb3c302093feb
0xD34DC0DE <emerickpoulin31415@gmail.com>
2021-10-08 20:02:14 -0400

    Experimental util to handle paging and validation

--- backend/src/main/java/com/team4/backend/service/InternshipOfferService.java
+++ backend/src/main/java/com/team4/backend/service/InternshipOfferService.java
@@ -7,8 +7,7 @@ import com.team4.backend.mapping.InternshipOfferMapper;
 import com.team4.backend.model.InternshipOffer;
 import com.team4.backend.model.Student;
 import com.team4.backend.repository.InternshipOfferRepository;
-import com.team4.backend.util.ValidatingPageRequest;
-import org.springframework.data.domain.PageRequest;
+import com.team4.backend.util.ExperimentalValidatingPageRequest;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
 import reactor.core.publisher.Flux;
@@ -43,13 +42,6 @@ public class InternshipOfferService {
     }
 
     public Flux<InternshipOffer> getStudentExclusiveOffers(String studentEmail, Integer page, Integer size) {
-        PageRequest pageRequest;
-        try {
-            pageRequest = new ValidatingPageRequest(page, size).getPageRequest();
-        } catch (InvalidPageRequestException e) {
-            return Flux.error(e);
-        }
-
         return studentService.getStudent(studentEmail)
                 .switchIfEmpty(
                         Mono.error(
@@ -57,42 +49,37 @@ public class InternshipOfferService {
                         )
                 )
                 .map(Student::getExclusiveOffersId)
-                .flatMapMany(offerIdList ->
-                        Flux.fromIterable(offerIdList)
-                                .skip(pageRequest.getOffset())
-                                .take(pageRequest.getPageSize())
-                                .flatMap(offerId ->
-                                        internshipOfferRepository.
-                                                findByIdAndIsExclusiveTrueAndLimitDateToApplyAfterAndIsValidatedTrue(
-                                                        offerId,
-                                                        LocalDate.now()
-                                                )
-                                )
-                ).delayElements(Duration.ofSeconds(1)); // TODO remove after demo
+                .flatMapMany(offerIdList -> ExperimentalValidatingPageRequest.applyPaging(offerIdList, page, size).log("PAGING")
+                .flatMap(offerId ->
+                        internshipOfferRepository.
+                                findByIdAndIsExclusiveTrueAndLimitDateToApplyAfterAndIsValidatedTrue(
+                                        offerId,
+                                        LocalDate.now()
+                                ).log("AFTER")
+                ).delayElements(Duration.ofMillis(20))); // TODO remove after demo
     }
 
     public Flux<InternshipOffer> getGeneralInternshipOffers(Integer page, Integer size) {
-
-        PageRequest pageRequest;
-        try {
-            pageRequest = new ValidatingPageRequest(page, size).getPageRequest();
-        } catch (InvalidPageRequestException e) {
-            return Flux.error(e);
-        }
-
-        return internshipOfferRepository
-                .findAllByIsExclusiveFalseAndLimitDateToApplyAfterAndIsValidatedTrue(LocalDate.now(), pageRequest);
+        return ExperimentalValidatingPageRequest.getPageRequestMono(page, size)
+                .flatMapMany(pageRequest ->
+                        internshipOfferRepository
+                                .findAllByIsExclusiveFalseAndLimitDateToApplyAfterAndIsValidatedTrue(
+                                        LocalDate.now(),
+                                        pageRequest
+                                )
+                );
 
     }
 
     public Flux<InternshipOffer> getNonValidatedInternshipOffers() {
         return internshipOfferRepository.findAllInternshipOfferByIsValidatedFalse();
     }
+
     public Flux<InternshipOffer> getNotYetValidatedInternshipOffers() {
         return internshipOfferRepository.findAllByValidationDateNullAndIsValidatedFalse();
     }
 
-    public Mono<InternshipOffer> validateInternshipOffer(String id){
+    public Mono<InternshipOffer> validateInternshipOffer(String id) {
         return internshipOfferRepository.findById(id).map(offer -> {
             offer.setIsValidated(true);
             offer.setValidationDate(LocalDateTime.now());
@@ -102,7 +89,7 @@ public class InternshipOfferService {
                         "Can't find InternshipOffer with this id.")));
     }
 
-    public Mono<InternshipOffer> refuseInternshipOffer(String id){
+    public Mono<InternshipOffer> refuseInternshipOffer(String id) {
         return internshipOfferRepository.findById(id).map(offer -> {
             offer.setIsValidated(false);
             offer.setValidationDate(LocalDateTime.now());
@@ -111,20 +98,21 @@ public class InternshipOfferService {
                 .switchIfEmpty(Mono.error(new ResponseStatusException(HttpStatus.NOT_FOUND,
                         "Can't find InternshipOffer with this id.")));
     }
+
     public Mono<Long> getInternshipOffersPageCount(Integer size) {
-        if(size < 1) {
+        if (size < 1) {
             return Mono.error(InvalidPageRequestException::new);
         }
 
         return internshipOfferRepository.countAllByIsExclusiveFalseAndLimitDateToApplyAfter(LocalDate.now())
-                .map(count -> (long)Math.ceil((double)count / (double)size));
+                .map(count -> (long) Math.ceil((double) count / (double) size));
     }
 
     public Mono<Long> getInternshipOffersPageCount(String studentEmail, Integer size) {
-        if(size < 1) {
+        if (size < 1) {
             return Mono.error(InvalidPageRequestException::new);
         }
-        if(studentEmail == null) {
+        if (studentEmail == null) {
             return Mono.error(new UserNotFoundException("Email is null"));
         }
 
@@ -133,6 +121,6 @@ public class InternshipOfferService {
                         Mono.error(new UserNotFoundException("Could not find student with email: " + studentEmail))
                 )
                 .map(student -> student.getExclusiveOffersId().size())
-                .map(count -> (long)Math.ceil((double)count / (double)size));
+                .map(count -> (long) Math.ceil((double) count / (double) size));
     }
 }
