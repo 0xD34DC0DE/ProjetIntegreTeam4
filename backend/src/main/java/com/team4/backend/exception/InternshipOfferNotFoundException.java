4d956ae681a6a6a6799794faf48c56e962905d9a
0xD34DC0DE <emerickpoulin31415@gmail.com>
2021-10-16 23:05:55 -0400

    EQ3-38: added status code to exceptions

--- backend/src/main/java/com/team4/backend/exception/InternshipOfferNotFoundException.java
+++ backend/src/main/java/com/team4/backend/exception/InternshipOfferNotFoundException.java
@@ -1,8 +1,11 @@
 package com.team4.backend.exception;
 
 import lombok.NoArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
 
 @NoArgsConstructor
+@ResponseStatus(code = HttpStatus.NOT_FOUND)
 public class InternshipOfferNotFoundException extends Exception {
 
     public InternshipOfferNotFoundException(String message) {
--- backend/src/main/java/com/team4/backend/exception/UnauthorizedException.java
+++ backend/src/main/java/com/team4/backend/exception/UnauthorizedException.java
@@ -1,5 +1,9 @@
 package com.team4.backend.exception;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(code = HttpStatus.UNAUTHORIZED)
 public class UnauthorizedException extends Exception {
     public UnauthorizedException() {
         super();
