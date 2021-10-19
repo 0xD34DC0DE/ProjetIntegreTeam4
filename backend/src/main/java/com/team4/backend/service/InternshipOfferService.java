1290dea077c0727cd23441bc470293efbdea1f98
0xD34DC0DE <emerickpoulin31415@gmail.com>
2021-10-18 22:33:00 -0400

    EQ3-38: set the hasAlreadyApplied boolean for offers

--- backend/src/main/java/com/team4/backend/service/InternshipOfferService.java
+++ backend/src/main/java/com/team4/backend/service/InternshipOfferService.java
@@ -1,6 +1,7 @@
 package com.team4.backend.service;
 
 import com.team4.backend.dto.InternshipOfferCreationDto;
+import com.team4.backend.dto.InternshipOfferStudentViewDto;
 import com.team4.backend.exception.InternshipOfferNotFoundException;
 import com.team4.backend.exception.InvalidPageRequestException;
 import com.team4.backend.exception.UnauthorizedException;
@@ -48,29 +49,44 @@ public class InternshipOfferService {
                         : Mono.error(new UserNotFoundException("Can't find monitor!")));
     }
 
-    public Flux<InternshipOffer> getStudentExclusiveOffers(String studentEmail, Integer page, Integer size) {
+    public Flux<InternshipOfferStudentViewDto> getStudentExclusiveOffers(String studentEmail, Integer page, Integer size) {
         return studentService.findByEmail(studentEmail)
-                .map(Student::getExclusiveOffersId)
-                .flatMapMany(offerIdList ->
-                        ValidatingPageRequest.applyPaging(offerIdList, page, size)
+                .flatMapMany(student ->
+                        ValidatingPageRequest.applyPaging(student.getExclusiveOffersId(), page, size)
                                 .flatMap(offerId ->
                                         internshipOfferRepository.
                                                 findByIdAndIsExclusiveTrueAndLimitDateToApplyAfterAndIsValidatedTrue(
                                                         offerId,
                                                         LocalDate.now()
                                                 )
-                                ));
+                                )
+                                .map(InternshipOfferMapper::toStudentViewDto)
+                                .map(internshipOfferDto -> {
+                                    if(student.getAppliedOffersId().contains(internshipOfferDto.getId())) {
+                                        internshipOfferDto.setHasAlreadyApplied(true);
+                                    }
+                                    return internshipOfferDto;
+                                })
+                );
     }
 
-    public Flux<InternshipOffer> getGeneralInternshipOffers(Integer page, Integer size) {
-        return ValidatingPageRequest.getPageRequestMono(page, size)
+    public Flux<InternshipOfferStudentViewDto> getGeneralInternshipOffers(Integer page, Integer size, Principal principal) {
+        return studentService.findByEmail(UserSessionService.getLoggedUserEmail(principal)).flatMapMany(student ->
+                ValidatingPageRequest.getPageRequestMono(page, size)
                 .flatMapMany(pageRequest ->
                         internshipOfferRepository
                                 .findAllByIsExclusiveFalseAndLimitDateToApplyAfterAndIsValidatedTrue(
                                         LocalDate.now(),
                                         pageRequest
                                 )
-                );
+                )
+                .map(InternshipOfferMapper::toStudentViewDto)
+                .flatMap(internshipOfferDto -> {
+                    if(student.getAppliedOffersId().contains(internshipOfferDto.getId())) {
+                        internshipOfferDto.setHasAlreadyApplied(true);
+                    }
+                    return Mono.just(internshipOfferDto);
+                }));
     }
 
     public Flux<InternshipOffer> getNotYetValidatedInternshipOffers() {
