4c85406fe66955c5059f7c4c9507cd77cfe7531c
Moussa <Moussa.Keita2411@gmail.com>
2021-10-18 21:46:05 -0400

    refactoring FileMetaDataService --> getListInvalidCvNotSeen to include ValidationgPageRequest and return right status when the parameters are invalid

--- backend/src/main/java/com/team4/backend/exception/InvalidPageRequestException.java
+++ backend/src/main/java/com/team4/backend/exception/InvalidPageRequestException.java
@@ -1,5 +1,8 @@
 package com.team4.backend.exception;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
 public class InvalidPageRequestException extends Exception {
     public InvalidPageRequestException(String message) {
         super(message);
