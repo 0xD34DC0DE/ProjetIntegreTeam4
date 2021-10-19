a3722981e7c0f9ad707c223c4714c06b58f7451c
0xD34DC0DE <emerickpoulin31415@gmail.com>
2021-10-18 20:52:19 -0400

    EQ3-38: use @NoArgsConstructor instead of implicit default constructor

--- backend/src/main/java/com/team4/backend/exception/UnauthorizedException.java
+++ backend/src/main/java/com/team4/backend/exception/UnauthorizedException.java
@@ -1,14 +1,12 @@
 package com.team4.backend.exception;
 
+import lombok.NoArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ResponseStatus;
 
+@NoArgsConstructor
 @ResponseStatus(code = HttpStatus.UNAUTHORIZED)
 public class UnauthorizedException extends Exception {
-    public UnauthorizedException() {
-        super();
-    }
-
     public UnauthorizedException(String message) {
         super(message);
     }
