c96c0a4008b477ac5703b9ca5997ece8f1f79b65
0xD34DC0DE <emerickpoulin31415@gmail.com>
2021-10-25 19:02:19 -0400

    EQ4-155: Added Internship mapper

---
+++ backend/src/main/java/com/team4/backend/mapping/InternshipMapper.java
@@ -0,0 +1,31 @@
+package com.team4.backend.mapping;
+
+import com.team4.backend.dto.InternshipCreationDto;
+import com.team4.backend.dto.InternshipDto;
+import com.team4.backend.model.Internship;
+import com.team4.backend.model.InternshipContract;
+
+public abstract class InternshipMapper {
+
+    public static Internship toEntity(InternshipCreationDto internshipCreationDto) {
+        return Internship.builder()
+                .monitorEmail(internshipCreationDto.getMonitorEmail())
+                .studentEmail(internshipCreationDto.getStudentEmail())
+                .internshipManagerEmail(internshipCreationDto.getInternshipManagerEmail())
+                .startDate(internshipCreationDto.getStartDate())
+                .endDate(internshipCreationDto.getEndDate())
+                .internshipContract(new InternshipContract())
+                .build();
+    }
+
+    public static InternshipDto toDto(Internship internship) {
+        return InternshipDto.builder()
+                .monitorEmail(internship.getMonitorEmail())
+                .studentEmail(internship.getStudentEmail())
+                .internshipManagerEmail(internship.getInternshipManagerEmail())
+                .startDate(internship.getStartDate())
+                .endDate(internship.getEndDate())
+                .build();
+    }
+
+}
