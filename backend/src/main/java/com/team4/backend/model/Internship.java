8463874a2ce1c13f51a144c829519c218153be56
0xD34DC0DE <emerickpoulin31415@gmail.com>
2021-10-25 19:01:13 -0400

    EQ4-155: Added Internship model

---
+++ backend/src/main/java/com/team4/backend/model/Internship.java
@@ -0,0 +1,47 @@
+package com.team4.backend.model;
+
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+import javax.persistence.Id;
+import java.time.LocalDate;
+
+@Data
+@NoArgsConstructor
+@Document(collection = "internships")
+public class Internship {
+
+    @Id
+    private String id;
+
+    private String monitorEmail;
+
+    private String internshipManagerEmail;
+
+    private String studentEmail;
+
+    private InternshipContract internshipContract;
+
+    private LocalDate startDate;
+
+    private LocalDate endDate;
+
+    @Builder
+    public Internship(String id,
+                      String monitorEmail,
+                      String internshipManagerEmail,
+                      String studentEmail,
+                      InternshipContract internshipContract,
+                      LocalDate startDate,
+                      LocalDate endDate) {
+        this.id = id;
+        this.monitorEmail = monitorEmail;
+        this.internshipManagerEmail = internshipManagerEmail;
+        this.studentEmail = studentEmail;
+        this.internshipContract = internshipContract;
+        this.startDate = startDate;
+        this.endDate = endDate;
+    }
+}
