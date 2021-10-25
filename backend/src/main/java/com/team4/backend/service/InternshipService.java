6ab085a0228f7a0e56ecc88f990d13f3988049de
0xD34DC0DE <emerickpoulin31415@gmail.com>
2021-10-25 19:03:22 -0400

    EQ4-155: Added Internship service

---
+++ backend/src/main/java/com/team4/backend/service/InternshipService.java
@@ -0,0 +1,50 @@
+package com.team4.backend.service;
+
+import com.team4.backend.dto.InternshipCreationDto;
+import com.team4.backend.model.Internship;
+import com.team4.backend.model.InternshipContract;
+import com.team4.backend.model.InternshipOffer;
+import com.team4.backend.model.Student;
+import com.team4.backend.repository.InternshipRepository;
+import org.springframework.stereotype.Service;
+import reactor.core.publisher.Mono;
+
+@Service
+public class InternshipService {
+
+    private final InternshipRepository internshipRepository;
+
+    private final InternshipOfferService internshipOfferService;
+
+    private final StudentService studentService;
+
+    public InternshipService(InternshipRepository internshipRepository,
+                             InternshipOfferService internshipOfferService,
+                             StudentService studentService) {
+        this.internshipRepository = internshipRepository;
+        this.internshipOfferService = internshipOfferService;
+        this.studentService = studentService;
+    }
+
+    public Mono<Internship> createInternship(InternshipCreationDto internshipCreationDto) {
+        Mono<Student> studentMono = studentService.findByEmail(internshipCreationDto.getStudentEmail());
+        Mono<InternshipOffer> internshipOfferMono = internshipOfferService
+                .findInternshipOfferById(internshipCreationDto.getInternshipOfferId());
+
+        return Mono.zip(studentMono, internshipOfferMono)
+                .map(tuple -> {
+                    Student student = tuple.getT1();
+                    InternshipOffer internshipOffer = tuple.getT2();
+
+                    return Internship.builder()
+                            .monitorEmail(internshipOffer.getEmailOfMonitor())
+                            .internshipManagerEmail(internshipOffer.getEmailOfApprovingInternshipManager())
+                            .studentEmail(student.getEmail())
+                            .internshipContract(new InternshipContract())
+                            .startDate(internshipOffer.getBeginningDate())
+                            .endDate(internshipOffer.getEndingDate())
+                            .build();
+                })
+                .flatMap(internshipRepository::save);
+    }
+}
