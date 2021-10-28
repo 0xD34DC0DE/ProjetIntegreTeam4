dab1eb7c141e48d7a3b0d12393b61af3f5aeb2d4
0xD34DC0DE <emerickpoulin31415@gmail.com>
2021-10-28 01:00:36 -0400

    EQ4-155: added contract signing dialog state

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -22,6 +22,7 @@ function App() {
     internshipOfferDialog: false,
     internshipOfferDialogValidation: false,
     emailSenderDialog: false,
+    signContractMonitorDialog: false,
   });
 
   const onSelectionChanged = (item) => {
