d1e512b263ccabc31d9dd6be72a06c3c430f7fab
0xD34DC0DE <emerickpoulin31415@gmail.com>
2021-10-30 15:35:48 -0400

    EQ4-166: Fixed Pdf presentation

--- frontend/src/components/PdfView.js
+++ frontend/src/components/PdfView.js
@@ -1,32 +1,75 @@
-import React, { useState, useContext } from "react";
+import React, { useState, useContext, useEffect } from "react";
 import { pdfjs } from "react-pdf";
-import { Document, Page } from 'react-pdf/dist/esm/entry.webpack';
+import { Document, Page } from "react-pdf/dist/esm/entry.webpack";
 import { UserInfoContext } from "../stores/UserInfoStore";
+import {
+  Pagination,
+  Container,
+  Grid,
+  Skeleton,
+} from "@mui/material";
 
 pdfjs.GlobalWorkerOptions.workerSrc = "pdf.worker.min.js";
 
-function PdfView({ pdfUrl }) {
+function PdfView({ pdfUrl, dialogRef }) {
   const [numPages, setNumPages] = useState(null);
   const [pageNumber, setPageNumber] = useState(1);
   const [userInfo] = useContext(UserInfoContext);
+  const [dialogHeight, setDialogHeight] = useState(700);
 
-  function onDocumentLoadSuccess({ numPages }) {
-    setNumPages(numPages);
-  }
+  const handlePageChange = (_, selectedPage) => {
+    setPageNumber(selectedPage);
+  };
+
+  useEffect(() => {
+    if (dialogRef.current) {
+      setDialogHeight(dialogRef.current.offsetHeight);
+    }
+  }, [dialogRef]);
+
+  const onDocumentLoadSuccess = (pdfProxy) => {
+    setNumPages(pdfProxy.numPages);
+  };
 
   return (
     <>
-      <div>
-        <Document
-          file={{ url: pdfUrl, httpHeaders: { 'Authorization': userInfo.jwt } }}
-          onLoadSuccess={onDocumentLoadSuccess}
-        >
-          <Page pageNumber={pageNumber} />
-        </Document>
-        <p>
-          Page {pageNumber} of {numPages}
-        </p>
-      </div>
+      <Container>
+          <Grid
+            container
+            direction="column"
+            justifyContent={{xs: "left", md:"center"}}
+            alignContent={{xs: "left", md:"center"}}
+            alignItems={{xs: "left", md:"center"}}
+          >
+            <Grid item xs={12} sm={11} md={10} lg={8} textAlign="center"> 
+              <Document
+                file={{
+                  url: pdfUrl,
+                  httpHeaders: { Authorization: userInfo.jwt },
+                }}
+                onLoadSuccess={onDocumentLoadSuccess}
+                loading={
+                  <Skeleton variant="rectangular" width={"100%"} height={500} />
+                }
+              >
+                <Page
+                  md={2}
+                  loading={null}
+                  pageNumber={pageNumber}
+                  height={dialogHeight * 0.7}
+                />
+              </Document>
+            </Grid>
+
+            <Grid item xs={12} textAlign="center">
+              <Pagination
+                count={numPages}
+                page={pageNumber}
+                onChange={handlePageChange}
+              />
+            </Grid>
+          </Grid>
+      </Container>
     </>
   );
 }
