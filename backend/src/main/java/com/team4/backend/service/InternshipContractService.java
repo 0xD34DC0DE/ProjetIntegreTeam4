debe27b64cf28623ffb175c7c4255d115c6381cc
0xD34DC0DE <emerickpoulin31415@gmail.com>
2021-10-30 20:50:20 -0400

    EQ4-23: implemented contract service

---
+++ backend/src/main/java/com/team4/backend/service/InternshipContractService.java
@@ -0,0 +1,89 @@
+package com.team4.backend.service;
+
+import com.team4.backend.dto.InternshipContractCreationDto;
+import com.team4.backend.model.*;
+import com.team4.backend.repository.InternshipContractRepository;
+import org.springframework.stereotype.Service;
+import reactor.core.publisher.Mono;
+
+import java.time.LocalDate;
+
+@Service
+public class InternshipContractService {
+
+    private final StudentService studentService;
+
+    private final MonitorService monitorService;
+
+    private final InternshipOfferService internshipOfferService;
+
+    private final InternshipManagerService internshipManagerService;
+
+    private final InternshipContractRepository internshipContractRepository;
+
+    public InternshipContractService(StudentService studentService,
+                                     MonitorService monitorService,
+                                     InternshipOfferService internshipOfferService, InternshipManagerService internshipManagerService,
+                                     InternshipContractRepository internshipContractRepository) {
+        this.studentService = studentService;
+        this.monitorService = monitorService;
+        this.internshipOfferService = internshipOfferService;
+        this.internshipManagerService = internshipManagerService;
+        this.internshipContractRepository = internshipContractRepository;
+    }
+
+    public Mono<InternshipContract> initiateContract(InternshipContractCreationDto internshipContractCreationDto) {
+        Mono<InternshipOffer> internshipOfferMono =
+                internshipOfferService.findInternshipOfferById(internshipContractCreationDto.getInternshipOfferId());
+
+        return internshipOfferMono.flatMap(internshipOffer ->
+                Mono.zip(
+                        internshipManagerService.findByEmail(internshipOffer.getEmailOfApprovingInternshipManager()),
+                        monitorService.findByEmail(internshipOffer.getMonitorEmail()),
+                        studentService.findByEmail(internshipContractCreationDto.getStudentEmail())
+                )
+                        .map(tuple -> buildInternshipContract(
+                                tuple.getT1(),
+                                tuple.getT2(),
+                                tuple.getT3(),
+                                internshipOffer)
+                        )
+                        .flatMap(internshipContractRepository::save));
+    }
+
+
+    private InternshipContract buildInternshipContract(InternshipManager internshipManager,
+                                                       Monitor monitor,
+                                                       Student student,
+                                                       InternshipOffer internshipOffer) {
+        Signature monitorSignature = Signature.builder()
+                .userId(monitor.getId())
+                .hasSigned(true)
+                .signDate(LocalDate.now())
+                .build();
+
+        Signature studentSignature = Signature.builder()
+                .userId(student.getId())
+                .hasSigned(false)
+                .build();
+
+        Signature internshipManagerSignature = Signature.builder()
+                .userId(internshipManager.getId())
+                .hasSigned(false)
+                .build();
+
+        return InternshipContract.builder()
+                .address("")
+                .startDate(internshipOffer.getBeginningDate())
+                .endDate(internshipOffer.getEndingDate())
+                .dailySchedule("")
+                .hourlyRate(0.0f)
+                .internTasks(internshipOffer.getDescription())
+                .studentSignature(studentSignature)
+                .monitorSignature(monitorSignature)
+                .internshipManagerSignature(internshipManagerSignature)
+                .build();
+    }
+
+
+}
