feb97f65b4e8e6bfb6de26d9dd0f2e5ed8834a6e
0xD34DC0DE <emerickpoulin31415@gmail.com>
2021-10-30 23:27:19 -0400

    EQ4-23: changed execution order

--- backend/src/main/java/com/team4/backend/TestingInserterRunner.java
+++ backend/src/main/java/com/team4/backend/TestingInserterRunner.java
@@ -22,7 +22,7 @@ import java.util.List;
 import java.util.Set;
 
 @Component
-@Order(1)
+@Order(2)
 public class TestingInserterRunner implements ApplicationRunner {
 
     private static final Logger log = LoggerFactory.getLogger(TestingInserterRunner.class);
