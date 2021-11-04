977e7f7fb08d7cc228e4cc06b27de0e35f80a522
Moussa <Moussa.Keita2411@gmail.com>
2021-11-04 15:16:03 -0400

    removing extra space in TaskSchedulerService

--- backend/src/main/java/com/team4/backend/service/TaskSchedulerService.java
+++ backend/src/main/java/com/team4/backend/service/TaskSchedulerService.java
@@ -28,6 +28,6 @@ public class TaskSchedulerService {
                                 nbrOfUpdateStudent +
                                 ", time : " +
                                 LocalDateTime.now()));
-
     }
+    
 }
