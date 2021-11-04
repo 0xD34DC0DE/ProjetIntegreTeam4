b2963530586dc47e9335b6960bc0f1f09a4d0df0
0xD34DC0DE <emerickpoulin31415@gmail.com>
2021-11-04 13:24:56 -0400

    EQ4-23: refactor and contract preview generation

--- backend/src/main/java/com/team4/backend/service/InternshipContractService.java
+++ backend/src/main/java/com/team4/backend/service/InternshipContractService.java
@@ -1,11 +1,17 @@
 package com.team4.backend.service;
 
+import com.team4.backend.InternshipManagerRegistration;
 import com.team4.backend.dto.InternshipContractCreationDto;
+import com.team4.backend.dto.InternshipContractDto;
 import com.team4.backend.model.*;
 import com.team4.backend.pdf.InternshipContractPdfTemplate;
 import com.team4.backend.repository.InternshipContractRepository;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
 import reactor.core.publisher.Mono;
+import reactor.util.function.Tuple3;
+import reactor.util.function.Tuple4;
 
 import java.time.LocalDate;
 import java.util.HashMap;
@@ -13,6 +19,8 @@ import java.util.Map;
 
 @Service
 public class InternshipContractService {
+
+    private static final Logger log = LoggerFactory.getLogger(InternshipContractService.class);
 
     private final StudentService studentService;
 
@@ -41,54 +49,91 @@ public class InternshipContractService {
     }
 
     public Mono<InternshipContract> initiateContract(InternshipContractCreationDto internshipContractCreationDto) {
+        return buildInternshipContractFromInternshipContractCreationDto(internshipContractCreationDto)
+                .flatMap(internshipContractRepository::save);
+    }
+
+    public Mono<byte[]> getContract(String internshipOfferId, String studentEmail) {
+
+        InternshipContractDto insternshipContractDto = InternshipContractDto.builder()
+                .internshipOfferId(internshipOfferId)
+                .studentEmail(studentEmail)
+                .build();
+
+        return studentService.findByEmail(studentEmail)
+                .flatMap(student -> internshipContractRepository.findInternshipContractByStudentId(student.getId()))
+                .switchIfEmpty(buildInternshipContractFromInternshipContractDto(insternshipContractDto))
+                .flatMap(this::getPdfBytes)
+                .onErrorMap(throwable -> {log.info(throwable.getLocalizedMessage()); return throwable;});
+    }
+
+    private Mono<InternshipContract> findContractByStudentEmail(String studentEmail) {
+        return studentService.findByEmail(studentEmail)
+                .flatMap(student ->
+                        internshipContractRepository.findInternshipContractByStudentId(student.getId())
+                );
+    }
+
+    private Mono<byte[]> getPdfBytes(InternshipContract internshipContract) {
+        return Mono.just(internshipContract)
+                .flatMap(this::getInternshipContractPdfTemplate)
+                .flatMap(pdfService::renderPdf);
+    }
+
+    private Mono<Tuple4<InternshipManager, Monitor, Student, InternshipOffer>> getContractObjectsTuple(
+            InternshipContractDto internshipContractDto) {
+
         Mono<InternshipOffer> internshipOfferMono =
-                internshipOfferService.findInternshipOfferById(internshipContractCreationDto.getInternshipOfferId());
+                internshipOfferService.findInternshipOfferById(internshipContractDto.getInternshipOfferId());
 
         return internshipOfferMono.flatMap(internshipOffer ->
                 Mono.zip(
                         internshipManagerService.findByEmail(internshipOffer.getEmailOfApprovingInternshipManager()),
                         monitorService.findByEmail(internshipOffer.getMonitorEmail()),
-                        studentService.findByEmail(internshipContractCreationDto.getStudentEmail())
+                        studentService.findByEmail(internshipContractDto.getStudentEmail()),
+                        Mono.just(internshipOffer)
                 )
-                        .map(tuple -> buildInternshipContract(
-                                tuple.getT1(),
-                                tuple.getT2(),
-                                tuple.getT3(),
-                                internshipOffer,
-                                internshipContractCreationDto)
-                        )
-                        .flatMap(internshipContractRepository::save));
+        );
     }
 
+    private Mono<InternshipContract> buildInternshipContractFromInternshipContractDto(
+            InternshipContractDto internshipContractDto) {
+
+        return getContractObjectsTuple(internshipContractDto)
+                .map(tuple -> buildInternshipContract(
+                        tuple.getT1(),
+                        tuple.getT2(),
+                        tuple.getT3(),
+                        tuple.getT4())
+                );
+
+    }
+
+    private Mono<InternshipContract> buildInternshipContractFromInternshipContractCreationDto(
+            InternshipContractCreationDto internshipContractCreationDto) {
+
+        return getContractObjectsTuple(internshipContractCreationDto)
+                .map(tuple -> buildSignedInternshipContract(
+                        tuple.getT1(),
+                        tuple.getT2(),
+                        tuple.getT3(),
+                        tuple.getT4(),
+                        internshipContractCreationDto)
+                );
+    }
 
     private InternshipContract buildInternshipContract(InternshipManager internshipManager,
                                                        Monitor monitor,
                                                        Student student,
-                                                       InternshipOffer internshipOffer,
-                                                       InternshipContractCreationDto internshipContractCreationDto) {
-        Signature monitorSignature = Signature.builder()
-                .userId(monitor.getId())
-                .hasSigned(true)
-                .signDate(LocalDate.now())
-                .build();
+                                                       InternshipOffer internshipOffer) {
 
-        Signature studentSignature = Signature.builder()
-                .userId(student.getId())
-                .hasSigned(false)
-                .build();
-
-        Signature internshipManagerSignature = Signature.builder()
-                .userId(internshipManager.getId())
-                .hasSigned(false)
-                .build();
+        Signature monitorSignature = buildSignature(monitor.getId(), false);
+        Signature studentSignature = buildSignature(student.getId(), false);
+        Signature internshipManagerSignature = buildSignature(internshipManager.getId(), false);
 
         return InternshipContract.builder()
-                .address(internshipContractCreationDto.getAddress())
                 .beginningDate(internshipOffer.getBeginningDate())
                 .endingDate(internshipOffer.getEndingDate())
-                .dailySchedule(internshipContractCreationDto.getDailySchedule())
-                .hoursPerWeek(internshipContractCreationDto.getHoursPerWeek())
-                .hourlyRate(internshipContractCreationDto.getHourlyRate())
                 .internTasks(internshipOffer.getDescription())
                 .studentSignature(studentSignature)
                 .monitorSignature(monitorSignature)
@@ -96,14 +141,39 @@ public class InternshipContractService {
                 .build();
     }
 
-    public Mono<byte[]> findContractByStudentEmail(String studentEmail) {
-        return studentService.findByEmail(studentEmail)
-                .flatMap(student -> internshipContractRepository.findInternshipContractByStudentId(student.getId()))
-                .flatMap(this::getInternshipContractPdfTemplate)
-                .flatMap(pdfService::renderPdf);
+    private Signature buildSignature(String id, boolean isSigned) {
+        return Signature.builder()
+                .userId(id)
+                .hasSigned(isSigned)
+                .signDate(isSigned ? LocalDate.now() : null)
+                .build();
     }
 
-    public Mono<InternshipContractPdfTemplate> getInternshipContractPdfTemplate(InternshipContract internshipContract) {
+    private InternshipContract buildSignedInternshipContract(
+            InternshipManager internshipManager,
+            Monitor monitor,
+            Student student,
+            InternshipOffer internshipOffer,
+            InternshipContractCreationDto internshipContractCreationDto) {
+
+        Signature monitorSignature = buildSignature(monitor.getId(), true);
+
+        InternshipContract internshipContract = buildInternshipContract(internshipManager,
+                monitor,
+                student,
+                internshipOffer);
+
+        internshipContract.setAddress(internshipContractCreationDto.getAddress());
+        internshipContract.setDailySchedule(internshipContractCreationDto.getDailySchedule());
+        internshipContract.setHoursPerWeek(internshipContractCreationDto.getHoursPerWeek());
+        internshipContract.setHourlyRate(internshipContractCreationDto.getHourlyRate());
+
+        internshipContract.setMonitorSignature(monitorSignature);
+
+        return internshipContract;
+    }
+
+    private Mono<InternshipContractPdfTemplate> getInternshipContractPdfTemplate(InternshipContract internshipContract) {
         return Mono.zip(
                 internshipManagerService.findById(internshipContract.getInternshipManagerSignature().getUserId()),
                 monitorService.findById(internshipContract.getMonitorSignature().getUserId()),
