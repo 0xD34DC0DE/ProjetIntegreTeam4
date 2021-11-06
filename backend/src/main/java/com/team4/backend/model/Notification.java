ffac117cdcd0fdb727391ab6935277614c850350
William Jean Mireault <williamjeanmireault@gmail.com>
2021-11-06 12:20:42 -0400

    Added notification model

---
+++ backend/src/main/java/com/team4/backend/model/Notification.java
@@ -0,0 +1,38 @@
+package ca.qc.claurendeau.backend.model;
+
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+import java.io.Serializable;
+import java.time.LocalDateTime;
+import java.util.Optional;
+
+@Data
+@NoArgsConstructor
+@Document(collection = "notifications")
+public class Notification implements Serializable {
+
+    @Id
+    private String id;
+    private String title;
+    private String content;
+    private String receiverEmail;
+    private LocalDateTime creationDate;
+
+    @Builder(builderMethodName = "notificationBuilder")
+    public Notification(String id,
+                        String title,
+                        String content,
+                        String receiverEmail,
+                        LocalDateTime creationDate) {
+        this.id = id;
+        this.title = title;
+        this.content = content;
+        this.receiverEmail = receiverEmail;
+        this.creationDate = Optional.ofNullable(creationDate).orElse(LocalDateTime.now());
+    }
+
+}
