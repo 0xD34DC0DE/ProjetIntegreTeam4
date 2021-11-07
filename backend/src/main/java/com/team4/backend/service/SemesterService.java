ffadc74d63b5f26849fbe8afa56b3d4751ace792
Moussa <Moussa.Keita2411@gmail.com>
2021-11-07 14:52:31 -0500

    EQ4-195: adding SemesterService --> findByFullName()

--- backend/src/main/java/com/team4/backend/service/SemesterService.java
+++ backend/src/main/java/com/team4/backend/service/SemesterService.java
@@ -1,9 +1,11 @@
 package com.team4.backend.service;
 
+import com.team4.backend.exception.SemesterNotFoundException;
 import com.team4.backend.model.Semester;
 import com.team4.backend.repository.SemesterRepository;
 import org.springframework.stereotype.Service;
 import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
 
 import java.util.List;
 
@@ -18,6 +20,11 @@ public class SemesterService {
 
     public Flux<Semester> initializeSemestersAnnually(List<Semester> semesters) {
         return semesterRepository.saveAll(semesters);
+    }
+
+    public Mono<Semester> findByFullName(String fullName){
+        return semesterRepository.findByFullName(fullName)
+                .switchIfEmpty(Mono.error(new SemesterNotFoundException("Can't find semester " + fullName)));
     }
 
 }
--- backend/src/test/java/com/team4/backend/service/SemesterServiceTest.java
+++ backend/src/test/java/com/team4/backend/service/SemesterServiceTest.java
@@ -1,18 +1,23 @@
 package com.team4.backend.service;
 
+import com.team4.backend.exception.SemesterNotFoundException;
 import com.team4.backend.model.Semester;
+import com.team4.backend.model.enums.SemesterName;
 import com.team4.backend.repository.SemesterRepository;
 import com.team4.backend.testdata.SemesterMockData;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
 import reactor.test.StepVerifier;
 
 import java.util.List;
 
+import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.when;
 
 @ExtendWith(MockitoExtension.class)
@@ -38,6 +43,37 @@ public class SemesterServiceTest {
         StepVerifier.create(semesterFlux)
                 .expectNextCount(3)
                 .verifyComplete();
+    }
+
+    @Test
+    void shouldFindSemesterByName() {
+        //ARRANGE
+        Semester semester = SemesterMockData.getListSemester().get(0);
+
+        when(semesterRepository.findByFullName(any())).thenReturn(Mono.just(semester));
+
+        //ACT
+        Mono<Semester> semesterMono = semesterRepository.findByFullName(semester.getFullName());
+
+        //ASSERT
+        StepVerifier.create(semesterMono)
+                .assertNext(s -> Assertions.assertEquals(semester.getFullName(), s.getFullName()))
+                .verifyComplete();
+    }
+
+    @Test
+    void shouldNotFindSemesterByName() {
+        //ARRANGE
+        Semester semester = SemesterMockData.getListSemester().get(0);
+
+        when(semesterRepository.findByFullName(any())).thenReturn(Mono.error(SemesterNotFoundException::new));
+
+        //ACT
+        Mono<Semester> semesterMono = semesterRepository.findByFullName(semester.getFullName());
+
+        //ASSERT
+        StepVerifier.create(semesterMono)
+                .verifyError(SemesterNotFoundException.class);
     }
 
 }
