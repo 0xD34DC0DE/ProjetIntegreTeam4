78bc046d8a853e0cf4742249ef7b6154b0d3bdb3
Moussa <Moussa.Keita2411@gmail.com>
2021-11-07 00:05:40 -0400

    semester test structure

--- backend/src/main/java/com/team4/backend/service/SemesterService.java
+++ backend/src/main/java/com/team4/backend/service/SemesterService.java
@@ -1,4 +1,16 @@
 package com.team4.backend.service;
 
+import com.team4.backend.repository.SemesterRepository;
+import org.springframework.stereotype.Service;
+
+@Service
 public class SemesterService {
+
+    private final SemesterRepository semesterRepository;
+
+    public SemesterService(SemesterRepository semesterRepository) {
+        this.semesterRepository = semesterRepository;
+    }
+
+
 }
--- backend/src/test/java/com/team4/backend/service/SemesterServiceTest.java
+++ backend/src/test/java/com/team4/backend/service/SemesterServiceTest.java
@@ -1,8 +1,28 @@
 package com.team4.backend.service;
 
+import com.team4.backend.repository.SemesterRepository;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 
 @ExtendWith(MockitoExtension.class)
 public class SemesterServiceTest {
+
+    @Mock
+    SemesterRepository semesterRepository;
+
+    @InjectMocks
+    SemesterService semesterService;
+
+    @Test
+    void shouldCreateSemester() {
+        //ARRANGE
+
+        //ACT
+
+        //ASSERT
+        
+    }
 }
