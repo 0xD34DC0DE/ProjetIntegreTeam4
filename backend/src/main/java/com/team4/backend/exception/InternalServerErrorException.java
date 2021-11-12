c7d1ae921b3e6a4a6d5ec9c22f167ad81db5a00a
0xD34DC0DE <emerickpoulin31415@gmail.com>
2021-11-12 17:38:30 -0500

    EQ4-22: Added internal server error exception

---
+++ backend/src/main/java/com/team4/backend/exception/InternalServerErrorException.java
@@ -0,0 +1,13 @@
+package com.team4.backend.exception;
+
+import lombok.NoArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@NoArgsConstructor
+@ResponseStatus(code = HttpStatus.INTERNAL_SERVER_ERROR)
+public class InternalServerErrorException extends Exception {
+    public InternalServerErrorException(String message) {
+        super(message);
+    }
+}
