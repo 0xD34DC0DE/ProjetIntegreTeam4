cab2008546351f4e981947bee11d1b52b2957cc5
0xD34DC0DE <emerickpoulin31415@gmail.com>
2021-11-12 17:39:51 -0500

    EQ4-22: Added method to get PDF by contract Id && verify signature by contract Id

--- backend/src/main/java/com/team4/backend/controller/InternshipContractController.java
+++ backend/src/main/java/com/team4/backend/controller/InternshipContractController.java
@@ -1,7 +1,11 @@
 package com.team4.backend.controller;
 
 import com.team4.backend.dto.InternshipContractCreationDto;
+import com.team4.backend.dto.InternshipContractDto;
+import com.team4.backend.security.UserSessionService;
 import com.team4.backend.service.InternshipContractService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
@@ -9,11 +13,21 @@ import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 import reactor.core.publisher.Mono;
 
+import java.security.Principal;
+
 @RestController
 @RequestMapping("/contract")
 public class InternshipContractController {
 
     private final InternshipContractService internshipContractService;
+
+    private static final Logger log = LoggerFactory.getLogger( InternshipContractController.class );
+    @ExceptionHandler
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    public void handle(Exception e) {
+        
+        log.warn("Returning HTTP 400 Bad Request", e);
+    }
 
     public InternshipContractController(InternshipContractService internshipContractService) {
         this.internshipContractService = internshipContractService;
@@ -27,11 +41,35 @@ public class InternshipContractController {
                 .map(c -> ResponseEntity.status(HttpStatus.CREATED).body(""));
     }
 
+    @PostMapping("/sign")
+    @PreAuthorize("hasAnyAuthority('STUDENT', 'INTERNSHIP_MANAGER')")
+    public Mono<ResponseEntity<String>> signContract(
+            @RequestBody InternshipContractDto internshipContractDto,
+            Principal principal) {
+        return internshipContractService
+                .signContract(internshipContractDto,
+                        UserSessionService.getLoggedUserEmail(principal)
+                )
+                .map(c -> ResponseEntity.status(HttpStatus.NO_CONTENT).body(""));
+    }
+
     @GetMapping(produces = MediaType.APPLICATION_PDF_VALUE)
     @PreAuthorize("hasAnyAuthority('MONITOR')")
     public Mono<byte[]> getContract(@RequestParam("internshipOfferId") String internshipOfferId,
                                     @RequestParam("studentEmail") String studentEmail) {
         return internshipContractService.getContract(internshipOfferId, studentEmail);
+    }
+
+    @GetMapping(path = "/byId/{contractId}", produces = MediaType.APPLICATION_PDF_VALUE)
+    @PreAuthorize("hasAnyAuthority('INTERSHIP_MANAGER', 'STUDENT')")
+    public Mono<byte[]> getContractById(@PathVariable("contractId") String contractId, Principal principal) {
+        return internshipContractService.getContractById(contractId, UserSessionService.getLoggedUserEmail(principal));
+    }
+
+    @GetMapping("/tmp")
+    @PreAuthorize("hasAnyAuthority('MONITOR', 'STUDENT', 'INTERNSHIP_MANAGER')")
+    public Mono<String> tmp() {
+        return internshipContractService.getContractIdTemporary();
     }
 
     @GetMapping("/signed")
@@ -40,6 +78,13 @@ public class InternshipContractController {
                                    @RequestParam("studentEmail") String studentEmail,
                                    @RequestParam("userEmail") String userEmail) {
         return internshipContractService.hasSigned(internshipOfferId, studentEmail, userEmail);
+    }
+
+    @GetMapping("/signedByContractId")
+    @PreAuthorize("hasAnyAuthority('MONITOR', 'STUDENT', 'INTERNSHIP_MANAGER')")
+    public Mono<Boolean> hasSignedByContractId(@RequestParam("contractId") String contractId,
+                                   @RequestParam("userEmail") String userEmail) {
+        return internshipContractService.hasSignedByContractId(contractId, userEmail);
     }
 
 }
