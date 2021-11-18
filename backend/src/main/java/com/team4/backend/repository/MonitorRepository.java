22ab3c9894f2e1de9d3a385e00dd9c5cd27b4216
Moussa <Moussa.Keita2411@gmail.com>
2021-11-17 21:23:47 -0500

    adding @Repository

--- backend/src/main/java/com/team4/backend/repository/MonitorRepository.java
+++ backend/src/main/java/com/team4/backend/repository/MonitorRepository.java
@@ -2,8 +2,10 @@ package com.team4.backend.repository;
 
 import com.team4.backend.model.Monitor;
 import org.springframework.data.mongodb.repository.ReactiveMongoRepository;
+import org.springframework.stereotype.Repository;
 import reactor.core.publisher.Mono;
 
+@Repository
 public interface MonitorRepository extends ReactiveMongoRepository<Monitor, String>, CustomMonitorRepository {
 
     Mono<Boolean> existsByEmailAndIsEnabledTrue(String email);
