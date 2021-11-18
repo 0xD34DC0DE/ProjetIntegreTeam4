8702ff14b1930e926f993e9872e0243f971b2353
0xD34DC0DE <emerickpoulin31415@gmail.com>
2021-11-17 20:51:57 -0500

    EQ4-22: use new implementation

--- backend/src/main/java/com/team4/backend/controller/NotificationController.java
+++ backend/src/main/java/com/team4/backend/controller/NotificationController.java
@@ -4,7 +4,6 @@ import com.team4.backend.dto.NotificationDto;
 import com.team4.backend.model.Notification;
 import com.team4.backend.service.NotificationService;
 import com.team4.backend.service.UserService;
-import lombok.extern.log4j.Log4j2;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.http.codec.ServerSentEvent;
@@ -14,7 +13,6 @@ import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 
 @RestController
-@Log4j2
 @RequestMapping("/notification")
 @PreAuthorize("hasAnyAuthority('STUDENT', 'MONITOR', 'INTERNSHIP_MANAGER', 'SUPERVISOR')")
 public class NotificationController {
@@ -30,7 +28,7 @@ public class NotificationController {
 
     @PostMapping("/create")
     public Mono<ResponseEntity<Notification>> createNotification(@RequestBody NotificationDto notificationDto) {
-        return notificationService.createNotification(notificationDto)
+        return notificationService.createNotifications(notificationDto)
                 .map(n -> ResponseEntity.status(HttpStatus.CREATED).body(n));
     }
 
