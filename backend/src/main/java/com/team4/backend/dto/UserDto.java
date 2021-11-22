03ba0eef9ff2de292cb838db9017c65ac18e023b
0xD34DC0DE <emerickpoulin31415@gmail.com>
2021-11-22 14:44:47 -0500

    add constructor for creating instances

--- backend/src/main/java/com/team4/backend/dto/UserDto.java
+++ backend/src/main/java/com/team4/backend/dto/UserDto.java
@@ -1,10 +1,12 @@
 package com.team4.backend.dto;
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
 @Data
 @NoArgsConstructor
+@AllArgsConstructor
 public class UserDto {
 
     protected String id;
