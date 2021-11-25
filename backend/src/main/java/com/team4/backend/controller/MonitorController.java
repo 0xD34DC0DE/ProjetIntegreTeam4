e2451c690f70fa232c8cd255c958c3ab48ac106b
Moussa <Moussa.Keita2411@gmail.com>
2021-11-25 17:13:49 -0500

    refactoring SupervisorCreationDto to SupervisorDetailsDto

--- backend/src/main/java/com/team4/backend/controller/MonitorController.java
+++ backend/src/main/java/com/team4/backend/controller/MonitorController.java
@@ -21,8 +21,8 @@ public class MonitorController {
         this.monitorService = monitorService;
     }
 
-    //TODO --> followConvention
     @PostMapping("/register")
+    //TODO --> followConvention MonitorCreationDto
     public Mono<ResponseEntity<String>> register(@RequestBody MonitorDetailsDto monitorDto) {
         return monitorService.registerMonitor(MonitorMapper.toEntity(monitorDto))
                 .flatMap(s -> Mono.just(ResponseEntity.status(HttpStatus.CREATED).body("")));
