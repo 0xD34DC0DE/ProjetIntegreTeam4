b57258b1546a541c6604a3acc95fbcdd56d49614
Maxime Dupuis <dupuismaxime@hotmail.com>
2021-12-01 13:17:23 -0500

    Update student state

--- frontend/src/components/Dashboard/SelectInterviewDate.js
+++ frontend/src/components/Dashboard/SelectInterviewDate.js
@@ -1,4 +1,4 @@
-import React, { useState,useContext } from "react";
+import React, { useState, useContext } from "react";
 import axios from "axios";
 import { Typography, TextField } from "@mui/material";
 import { motion } from "framer-motion";
@@ -63,6 +63,7 @@ const SelectInterviewDate = ({
         !hasInternship())
     ) {
       setProfile({ ...profile, closestInterviewDate: value });
+      setProfile({ ...profile, studentState: "WAITING_INTERVIEW" });
       updateInterviewDate(value);
       setIsDateValid(true);
     } else {
