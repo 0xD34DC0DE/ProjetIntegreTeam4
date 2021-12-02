cbb7205548731685b150e2b2861f0d004f486e14
William Jean Mireault <williamjeanmireault@gmail.com>
2021-12-02 14:37:11 -0500

    Added download button for evaluation form, removed expection in controller

--- frontend/src/components/Evaluation/EvaluationDialogPreview.js
+++ frontend/src/components/Evaluation/EvaluationDialogPreview.js
@@ -1,11 +1,17 @@
-import { Dialog, DialogContent, Grid } from "@mui/material";
-import React, { useContext } from "react";
+import { Dialog, DialogContent, Grid, Button } from "@mui/material";
+import React, { useContext, useState, useEffect } from "react";
 import { DialogContext } from "../../stores/DialogStore.js";
+import { UserInfoContext } from "../../stores/UserInfoStore";
+import DownloadIcon from "@mui/icons-material/Download";
 import PdfView from "../PdfView.js";
+import axios from "axios";
 
 function EvaluationDialogPreview({ evaluationId, setEvaluationId, mid }) {
   const [dialog, dialogDispatch] = useContext(DialogContext);
-  const handleClose = (_, reason) => {
+  const [userInfo] = useContext(UserInfoContext);
+  const [pdfBlobUrl, setPdfBlobUrl] = useState("");
+
+  const handleClose = () => {
     dialogDispatch({
       type: "CLOSE",
       dialogName: "evaluationDialogPreview",
@@ -20,6 +26,30 @@ function EvaluationDialogPreview({ evaluationId, setEvaluationId, mid }) {
       evaluationId
     );
   };
+
+  const createPdfUrl = () => {
+    axios({
+      method: "GET",
+      headers: {
+        Authorization: userInfo.jwt,
+      },
+      url: getPdfUrl(),
+      responseType: "arraybuffer",
+    }).then((response) => {
+      const pdfBlob = new Blob([response.data], { type: "application/pdf" });
+      setPdfBlobUrl(URL.createObjectURL(pdfBlob));
+    });
+  };
+
+  const openPdfUrl = () => {
+    const newWindow = window.open(pdfBlobUrl, "_blank", "noopener,noreferrer");
+    if (newWindow) newWindow.opener = null;
+  };
+
+  useEffect(() => {
+    if (pdfBlobUrl === "") return;
+    openPdfUrl();
+  }, [pdfBlobUrl]);
 
   return (
     <>
@@ -40,6 +70,26 @@ function EvaluationDialogPreview({ evaluationId, setEvaluationId, mid }) {
               <>
                 <Grid item xs={12}>
                   <PdfView pdfUrl={getPdfUrl()} params={{}} />
+                </Grid>
+                <Grid item textAlign="center" xs={11}>
+                  <Button
+                    variant="contained"
+                    sx={{
+                      color: "white !important",
+                      backgroundColor: "rgba(125, 51, 235, 0.8) !important",
+                      ":hover": {
+                        backgroundColor: "rgba(95, 21, 205, 0.6) !important",
+                      },
+                      fontSize: "0.9em",
+                      boxShadow: "2px 2px 10px 2px rgba(0, 0, 0, 0.5)",
+                    }}
+                    onClick={() => {
+                      createPdfUrl();
+                    }}
+                  >
+                    Télécharger
+                    <DownloadIcon />
+                  </Button>
                 </Grid>
               </>
             </Grid>
