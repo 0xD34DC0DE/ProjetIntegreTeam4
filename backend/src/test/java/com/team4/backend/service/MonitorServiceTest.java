a178b30f9255bbf0659994d035e016845afe13e8
Moussa <Moussa.Keita2411@gmail.com>
2021-12-01 19:29:47 -0500

    adding 4 missing test for findBy

--- backend/src/test/java/com/team4/backend/service/MonitorServiceTest.java
+++ backend/src/test/java/com/team4/backend/service/MonitorServiceTest.java
@@ -1,6 +1,7 @@
 package com.team4.backend.service;
 
 import com.team4.backend.exception.UserAlreadyExistsException;
+import com.team4.backend.exception.UserNotFoundException;
 import com.team4.backend.model.Monitor;
 import com.team4.backend.repository.MonitorRepository;
 import com.team4.backend.testdata.MonitorMockData;
@@ -11,12 +12,12 @@ import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
+import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 import reactor.test.StepVerifier;
 
-import static org.junit.jupiter.api.Assertions.assertNotEquals;
-import static org.junit.jupiter.api.Assertions.assertNotNull;
-import static org.mockito.ArgumentMatchers.any;
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.ArgumentMatchers.*;
 import static org.mockito.Mockito.when;
 
 @ExtendWith(MockitoExtension.class)
@@ -86,7 +87,7 @@ public class MonitorServiceTest {
         Mono<Monitor> monitorMono = monitorService.registerMonitor(monitor);
 
         //ASSERT
-        StepVerifier.create(monitorMono).consumeNextWith(m -> {
+        StepVerifier.create(monitorMono).assertNext(m -> {
             assertNotNull(m.getId());
             assertNotEquals(MonitorMockData.getMockMonitor().getPassword(), m.getPassword());
         }).verifyComplete();
@@ -105,6 +106,78 @@ public class MonitorServiceTest {
 
         //ASSERT
         StepVerifier.create(monitorMono).expectError(UserAlreadyExistsException.class).verify();
+    }
+
+    @Test
+    void shouldFindByEmail() {
+        //ARRANGE
+        Monitor monitor = MonitorMockData.getMockMonitor();
+
+        when(monitorRepository.findByEmail(anyString())).thenReturn(Mono.just(monitor));
+
+        //ACT
+        Mono<Monitor> monitorMono = monitorService.findByEmail(monitor.getEmail());
+
+        //ASSERT
+        StepVerifier.create(monitorMono)
+                .assertNext(m -> assertEquals(monitor.getEmail(), m.getEmail()))
+                .verifyComplete();
+    }
+
+    @Test
+    void shouldNotFindByEmail() {
+        //ARRANGE
+        when(monitorRepository.findByEmail(anyString())).thenReturn(Mono.error(UserNotFoundException::new));
+
+        //ACT
+        Mono<Monitor> monitorMono = monitorService.findByEmail(anyString());
+
+        //ASSERT
+        StepVerifier.create(monitorMono)
+                .verifyError(UserNotFoundException.class);
+    }
+
+    @Test
+    void shouldFindById() {
+        //ARRANGE
+        Monitor monitor = MonitorMockData.getMockMonitor();
+
+        when(monitorRepository.findById(anyString())).thenReturn(Mono.just(monitor));
+
+        //ACT
+        Mono<Monitor> monitorMono = monitorService.findById(monitor.getId());
+
+        //ASSERT
+        StepVerifier.create(monitorMono)
+                .assertNext(m -> assertEquals(monitor.getId(), m.getId()))
+                .verifyComplete();
+    }
+
+    @Test
+    void shouldNotFindById() {
+        //ARRANGE
+        when(monitorRepository.findById(anyString())).thenReturn(Mono.error(UserNotFoundException::new));
+
+        //ACT
+        Mono<Monitor> monitorMono = monitorService.findById(anyString());
+
+        //ASSERT
+        StepVerifier.create(monitorMono)
+                .verifyError(UserNotFoundException.class);
+    }
+
+    @Test
+    void shouldFindBAllByIds() {
+        //ARRANGE
+        when(monitorRepository.findAllByIds(anyList())).thenReturn(Flux.just(MonitorMockData.getMockMonitor()));
+
+        //ACT
+        Flux<Monitor> monitorFlux = monitorService.findAllByIds(anyList());
+
+        //ASSERT
+        StepVerifier.create(monitorFlux)
+                .expectNextCount(1)
+                .verifyComplete();
     }
 
 }
